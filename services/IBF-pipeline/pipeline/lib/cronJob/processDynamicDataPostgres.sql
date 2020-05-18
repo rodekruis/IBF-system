b372bd952f99dcee1121188ced86f9c6a4c1b062
Jannis Visser <jannisvisser@redcross.nl>
2020-05-18 09:27:30 +0200

    T4956 Zambia-pipeline running + new db/mailchimp

--- services/IBF-pipeline/pipeline/lib/cronJob/processDynamicDataPostgres.sql
+++ services/IBF-pipeline/pipeline/lib/cronJob/processDynamicDataPostgres.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS zmb_fbf.dashboard_forecast_per_station;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_forecast_per_station;
 SELECT t0.station_code
 	,t0.station_name
 	,t0.trigger_level
@@ -12,13 +12,13 @@ SELECT t0.station_code
 	,t0.geom
 	,other_lead_time_trigger
 	,case when t3.station is null then 0 else 1 end as station_used
-INTO zmb_fbf.dashboard_forecast_per_station 
-FROM zmb_fbf.dashboard_glofas_stations_v2 t0
-LEFT JOIN zmb_fbf.triggers_rp_per_station_short t1
+INTO "IBF-pipeline-output".dashboard_forecast_per_station 
+FROM "IBF-pipeline-output".dashboard_glofas_stations_v2 t0
+LEFT JOIN "IBF-pipeline-output".triggers_rp_per_station_short t1
 ON t0.station_code = t1.station_code
-LEFT JOIN (select to_date(date,'yyyy-mm-dd') as current_prev, max(fc_long_trigger) as other_lead_time_trigger from zmb_fbf.triggers_rp_per_station_long group by 1) t2
+LEFT JOIN (select to_date(date,'yyyy-mm-dd') as current_prev, max(fc_long_trigger) as other_lead_time_trigger from "IBF-pipeline-output".triggers_rp_per_station_long group by 1) t2
 ON to_date(date,'yyyy-mm-dd') = t2.current_prev
-LEFT JOIN (select "station_code_3day" as station from zmb_fbf.waterstation_per_district group by 1) t3
+LEFT JOIN (select "station_code_3day" as station from "IBF-pipeline-output".waterstation_per_district group by 1) t3
 ON t3.station = t0.station_code
 where to_date(date,'yyyy-mm-dd') >= current_date - 1
 
@@ -37,17 +37,17 @@ SELECT t0.station_code
 	,t0.geom
 	,other_lead_time_trigger
 	,case when t3.station is null then 0 else 1 end as station_used
-FROM zmb_fbf.dashboard_glofas_stations_v2 t0
-LEFT JOIN zmb_fbf.triggers_rp_per_station_long t1
+FROM "IBF-pipeline-output".dashboard_glofas_stations_v2 t0
+LEFT JOIN "IBF-pipeline-output".triggers_rp_per_station_long t1
 ON t0.station_code = t1.station_code
-LEFT JOIN (select to_date(date,'yyyy-mm-dd') as current_prev, max(fc_short_trigger) as other_lead_time_trigger from zmb_fbf.triggers_rp_per_station_short group by 1) t2
+LEFT JOIN (select to_date(date,'yyyy-mm-dd') as current_prev, max(fc_short_trigger) as other_lead_time_trigger from "IBF-pipeline-output".triggers_rp_per_station_short group by 1) t2
 ON to_date(date,'yyyy-mm-dd') = t2.current_prev
-LEFT JOIN (select "station_code_7day" as station from zmb_fbf.waterstation_per_district group by 1) t3
+LEFT JOIN (select "station_code_7day" as station from "IBF-pipeline-output".waterstation_per_district group by 1) t3
 ON t3.station = t0.station_code
 where to_date(date,'yyyy-mm-dd') >= current_date - 1
 ;
 
-DROP TABLE IF EXISTS zmb_fbf.dashboard_forecast_per_district;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_forecast_per_district;
 select case when length(cast(pcode as varchar)) = 3 then '0' || cast(pcode as varchar) else cast(pcode as varchar) end as pcode
 	,case when lead_time = '3-day' then "station_code_3day" when lead_time = '7-day' then "station_code_7day" end as station_code
 	,lead_time
@@ -55,13 +55,13 @@ select case when length(cast(pcode as varchar)) = 3 then '0' || cast(pcode as va
 	,current_prev
 	,fc,fc_trigger,fc_rp,fc_perc,fc_prob,fc_trigger2
 	,other_lead_time_trigger
-INTO zmb_fbf.dashboard_forecast_per_district 
-FROM zmb_fbf.waterstation_per_district t0
-LEFT JOIN zmb_fbf.dashboard_forecast_per_station  t1
+INTO "IBF-pipeline-output".dashboard_forecast_per_district 
+FROM "IBF-pipeline-output".waterstation_per_district t0
+LEFT JOIN "IBF-pipeline-output".dashboard_forecast_per_station  t1
 ON (t1.lead_time = '7-day' and t0."station_code_7day" = t1.station_code) OR (t1.lead_time = '3-day' and t0."station_code_3day" = t1.station_code)
 ;
 
-DROP TABLE IF EXISTS zmb_fbf.dashboard_calculated_affected_adm3;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_calculated_affected_adm3;
 select *
 ,cattle_affected * 1 +
 goat_affected * 0.1 + 
@@ -69,7 +69,7 @@ sheep_affected * 0.1 +
 pig_affected * 0.28 + 
 chicken_affected * 0.01
 as livestock_affected
-INTO zmb_fbf.dashboard_calculated_affected_adm3 
+INTO "IBF-pipeline-output".dashboard_calculated_affected_adm3 
 from (
 select pcode
     ,date
@@ -93,10 +93,10 @@ from (
 	    else cast(sum as float) 
 	    end as affected
 	, t1b.fc_trigger
-    FROM zmb_fbf.calculated_affected_short t0
-    left join zmb_fbf.pcode_mapping_wards_new_distcode t1a
+    FROM "IBF-pipeline-output".calculated_affected_short t0
+    left join "IBF-pipeline-output".pcode_mapping_wards_new_distcode t1a
 	on t0.district = t1a.pcode
-    left join zmb_fbf.dashboard_forecast_per_district t1b
+    left join "IBF-pipeline-output".dashboard_forecast_per_district t1b
 	on case when length(cast(t1a.pcode_level2_new as varchar)) = 3 then '0' || cast(t1a.pcode_level2_new as varchar) else cast(t1a.pcode_level2_new as varchar) end = t1b.pcode 
 	and t1b.lead_time = '3-day'
 	and t0.date = t1b.date
@@ -114,10 +114,10 @@ from (
 	    else cast(sum as float) 
 	    end as affected
 	, t1b.fc_trigger
-    FROM zmb_fbf.calculated_affected_long t0
-    left join zmb_fbf.pcode_mapping_wards_new_distcode t1a
+    FROM "IBF-pipeline-output".calculated_affected_long t0
+    left join "IBF-pipeline-output".pcode_mapping_wards_new_distcode t1a
 	on t0.district = t1a.pcode
-    left join zmb_fbf.dashboard_forecast_per_district t1b
+    left join "IBF-pipeline-output".dashboard_forecast_per_district t1b
 	on case when length(cast(t1a.pcode_level2_new as varchar)) = 3 then '0' || cast(t1a.pcode_level2_new as varchar) else cast(t1a.pcode_level2_new as varchar) end = t1b.pcode 
 	and t1b.lead_time = '7-day'
 	and t0.date = t1b.date
@@ -127,22 +127,22 @@ group by 1,2,3,4
 ) bb
 ;
 
-DROP TABLE IF EXISTS zmb_fbf.data_adm3;
+DROP TABLE IF EXISTS "IBF-pipeline-output".data_adm3;
 select t1.*
 	,fc_trigger
 	,fc_trigger2
-into zmb_fbf.data_adm3
-from "ZMB_datamodel"."Indicators_3_TOTAL" t0
-left join zmb_fbf.dashboard_calculated_affected_adm3 t1
+into "IBF-pipeline-output".data_adm3
+from "IBF-static-input"."ZMB_CRA_Indicators_3" t0
+left join "IBF-pipeline-output".dashboard_calculated_affected_adm3 t1
 	ON t0.pcode = t1.pcode	
-left join zmb_fbf.dashboard_forecast_per_district t2
+left join "IBF-pipeline-output".dashboard_forecast_per_district t2
 	ON t0.pcode_parent = t2.pcode and t1.current_prev = t2.current_prev and t1.lead_time = t2.lead_time
 ;
 
-drop table if exists zmb_fbf.help_table;
+drop table if exists "IBF-pipeline-output".help_table;
 select 'Current' as current_prev
 ,'3-day' as lead_time
-into zmb_fbf.help_table
+into "IBF-pipeline-output".help_table
 union all
 select 'Previous' as current_prev
 ,'3-day' as lead_time
@@ -155,7 +155,7 @@ select 'Previous' as current_prev
 ;
 
 
-DROP TABLE IF EXISTS zmb_fbf.data_adm2;
+DROP TABLE IF EXISTS "IBF-pipeline-output".data_adm2;
 select t0.pcode
 	,t3.date
 	,t0a.current_prev
@@ -166,13 +166,13 @@ select t0.pcode
 	,coalesce(livestock_affected,0) as livestock_affected
 	,chicken_affected,cattle_affected,goat_affected,pig_affected,sheep_affected
 	,coalesce(cropland_affected,0) as cropland_affected
-into zmb_fbf.data_adm2
-from "ZMB_datamodel"."Indicators_2_TOTAL" t0
-left join zmb_fbf.help_table t0a
+into "IBF-pipeline-output".data_adm2
+from "IBF-static-input"."ZMB_CRA_Indicators_2" t0
+left join "IBF-pipeline-output".help_table t0a
 on 1=1
-left join zmb_fbf.waterstation_per_district t1
+left join "IBF-pipeline-output".waterstation_per_district t1
 on t0.pcode = case when length(cast(t1.pcode as varchar)) = 3 then '0' || cast(t1.pcode as varchar) else cast(t1.pcode as varchar) end
-left join zmb_fbf.dashboard_forecast_per_station  t2
+left join "IBF-pipeline-output".dashboard_forecast_per_station  t2
 ON ((t2.lead_time = '7-day' and t0a.lead_time = t2.lead_time and t1."station_code_7day" = t2.station_code) OR (t2.lead_time = '3-day' and t0a.lead_time = t2.lead_time and t1."station_code_3day" = t2.station_code))
 and t0a.current_prev = t2.current_prev
 and t0a.lead_time = t2.lead_time
@@ -190,19 +190,19 @@ select
     ,sum(sheep_affected) as sheep_affected
     ,sum(livestock_affected) as livestock_affected
     ,sum(cropland_affected) as cropland_affected
-from zmb_fbf.dashboard_calculated_affected_adm3 t0
-left join zmb_fbf.pcode_mapping_wards_new_distcode t1
+from "IBF-pipeline-output".dashboard_calculated_affected_adm3 t0
+left join "IBF-pipeline-output".pcode_mapping_wards_new_distcode t1
     on t0.pcode = case when length(cast(t1.pcode as varchar)) = 8 then '0' || cast(t1.pcode as varchar) else cast(t1.pcode as varchar) end
 group by 1,2,3,4
 ) t3
 ON t0.pcode = t3.pcode_level2 and t0a.lead_time = t3.lead_time and t0a.current_prev = t3.current_prev
 ;
 
-DROP TABLE IF EXISTS zmb_fbf.data_adm1;
+DROP TABLE IF EXISTS "IBF-pipeline-output".data_adm1;
 select t1.fc_trigger,t1.fc_trigger2
 	,t2.*
-into zmb_fbf.data_adm1
-from "ZMB_datamodel"."Indicators_1_TOTAL" t0
+into "IBF-pipeline-output".data_adm1
+from "IBF-static-input"."ZMB_CRA_Indicators_1" t0
 left join (
 select
     substring(case when length(cast(t1.pcode_level2_new as varchar)) = 3 then '0' || cast(t1.pcode_level2_new as varchar) else cast(t1.pcode_level2_new as varchar) end,1,2) as pcode
@@ -217,8 +217,8 @@ select
     ,sum(sheep_affected) as sheep_affected
     ,sum(livestock_affected) as livestock_affected
     ,sum(cropland_affected) as cropland_affected
-from zmb_fbf.dashboard_calculated_affected_adm3 t0
-left join zmb_fbf.pcode_mapping_wards_new_distcode t1
+from "IBF-pipeline-output".dashboard_calculated_affected_adm3 t0
+left join "IBF-pipeline-output".pcode_mapping_wards_new_distcode t1
     on t0.pcode = case when length(cast(t1.pcode as varchar)) = 8 then '0' || cast(t1.pcode as varchar) else cast(t1.pcode as varchar) end
 group by 1,2,3,4
 ) t2
@@ -230,7 +230,7 @@ select substring(pcode,1,2) as pcode
     ,date
     ,max(fc_trigger) as fc_trigger
     ,max(fc_trigger2) as fc_trigger2
-from zmb_fbf.data_adm2 
+from "IBF-pipeline-output".data_adm2 
 group by 1,2,3,4
 ) t1
 on t2.pcode = t1.pcode and t2.lead_time = t1.lead_time and t2.date = t1.date
--- services/IBF-pipeline/pipeline/lib/setup/processStaticDataPostgres.sql
+++ services/IBF-pipeline/pipeline/lib/setup/processStaticDataPostgres.sql
@@ -1,55 +1,55 @@
-DROP TABLE IF EXISTS zmb_fbf.dashboard_glofas_stations_v2;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_glofas_stations_v2;
 SELECT station_code
 	, station_name
 	, "10yr_threshold" as trigger_level
 			,st_SetSrid(st_MakePoint(lat, lon), 4326) as geom
-INTO zmb_fbf.dashboard_glofas_stations_v2
-FROM zmb_fbf.glofas_stations
+INTO "IBF-pipeline-output".dashboard_glofas_stations_v2
+FROM "IBF-pipeline-output".glofas_stations
 ;
-DROP TABLE IF EXISTS zmb_fbf.dashboard_redcross_branches;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_redcross_branches;
 SELECT  "BRANCH" branch_name
 	, "PROVINCE" province
 	, "PRESIDENT" president
 	, "TOTAL" as nr_volunteers
 	, "LOCATION OF OFFICE" as address
 			,st_SetSrid(st_MakePoint(lat, lon), 4326) as geom
-INTO zmb_fbf.dashboard_redcross_branches
-FROM zmb_fbf.redcross_branches
+INTO "IBF-pipeline-output".dashboard_redcross_branches
+FROM "IBF-pipeline-output".redcross_branches
 ;
 
-DROP TABLE IF EXISTS zmb_fbf.dashboard_poi_healthsites;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_poi_healthsites;
 SELECT name
 			,type
 			,st_SetSrid(st_MakePoint("Y", "X"), 4326) as geom
-INTO zmb_fbf.dashboard_poi_healthsites
-FROM zmb_fbf.healthsites;
+INTO "IBF-pipeline-output".dashboard_poi_healthsites
+FROM "IBF-pipeline-output".healthsites;
 
-DROP TABLE IF EXISTS zmb_fbf.dashboard_poi_waterpoints;
+DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_poi_waterpoints;
 SELECT water_tech
 	,activity_id
 			,st_SetSrid(st_MakePoint(lat_deg, lon_deg), 4326) as geom
-INTO zmb_fbf.dashboard_poi_waterpoints
-FROM zmb_fbf.waterpoints
+INTO "IBF-pipeline-output".dashboard_poi_waterpoints
+FROM "IBF-pipeline-output".waterpoints
 ;
 
-DROP TABLE IF EXISTS zmb_fbf.dashboard_roads;
-SELECT row_to_json(featcoll) as roads
-into zmb_fbf.dashboard_roads
-FROM (
-	SELECT 'FeatureCollection' As type, array_to_json(array_agg(feat)) As features
-	FROM (
-		SELECT 'Feature' As type
-			,ST_AsGeoJSON(tbl.geom)::json As geometry
-			,row_to_json((SELECT l FROM (SELECT tbl.highway) As l)) As properties
-		FROM zmb_fbf.hotosm_zmb_roads_lines_mapshaper As tbL
-		WHERE highway in ('primary','secondary','tertiary')
-		)  As feat 
-	)  As featcoll
-;
+--DROP TABLE IF EXISTS "IBF-pipeline-output".dashboard_roads;
+--SELECT row_to_json(featcoll) as roads
+--into "IBF-pipeline-output".dashboard_roads
+--FROM (
+--	SELECT 'FeatureCollection' As type, array_to_json(array_agg(feat)) As features
+--	FROM (
+--		SELECT 'Feature' As type
+--			,ST_AsGeoJSON(tbl.geom)::json As geometry
+--			,row_to_json((SELECT l FROM (SELECT tbl.highway) As l)) As properties
+--		FROM "IBF-pipeline-output".hotosm_zmb_roads_lines_mapshaper As tbL
+--		WHERE highway in ('primary','secondary','tertiary')
+--		)  As feat 
+--	)  As featcoll
+--;
 
 
-DROP FUNCTION IF EXISTS usp_fbf_geodata(varchar,varchar);
-CREATE OR REPLACE FUNCTION usp_fbf_geodata(country varchar, table_name varchar, OUT result json) AS $func$
+DROP FUNCTION IF EXISTS "IBF-pipeline-output".usp_fbf_geodata(varchar,varchar);
+CREATE OR REPLACE FUNCTION "IBF-pipeline-output".usp_fbf_geodata(country varchar, table_name varchar, OUT result json) AS $func$
 	BEGIN
 	EXECUTE format('
 		SELECT row_to_json(featcoll)
@@ -67,8 +67,8 @@ CREATE OR REPLACE FUNCTION usp_fbf_geodata(country varchar, table_name varchar, 
 	END
 $func$ LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS usp_fbf_data(varchar, varchar);
-CREATE OR REPLACE FUNCTION usp_fbf_data(country varchar, table_name varchar, OUT result json) AS $func$
+DROP FUNCTION IF EXISTS "IBF-pipeline-output".usp_fbf_data(varchar, varchar);
+CREATE OR REPLACE FUNCTION "IBF-pipeline-output".usp_fbf_data(country varchar, table_name varchar, OUT result json) AS $func$
 	BEGIN
 	EXECUTE format('select array_to_json(array_agg(tbl))
 			from (
