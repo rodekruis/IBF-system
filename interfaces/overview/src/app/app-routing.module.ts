d7521a0179a61a9e853f604234d499c16c91d9dd
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-11 16:04:05 +0200

    Add login-page

--- interfaces/overview/src/app/app-routing.module.ts
+++ interfaces/overview/src/app/app-routing.module.ts
@@ -3,14 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadChildren: () =>
       import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
   },
 ];
 
