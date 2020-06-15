8f4af843147bebb0ac5fab263b2dcd4a6a91c907
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-15 12:47:21 +0200

    Move login-page into /pages

--- interfaces/overview/src/app/app-routing.module.ts
+++ interfaces/overview/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () =>
-      import('./login/login.module').then((m) => m.LoginPageModule),
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'map',
--- interfaces/overview/src/app/pages/login/login.module.ts
+++ interfaces/overview/src/app/pages/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { SharedModule } from '../shared.module';
+import { SharedModule } from '../../shared.module';
 import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 
