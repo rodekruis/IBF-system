757b5251117d2306df62ed86c3ea81b81f1b9765
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-15 12:48:05 +0200

    Move /home into /pages

--- interfaces/overview/src/app/app-routing.module.ts
+++ interfaces/overview/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: 'login',
