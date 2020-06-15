06b7fca8d0c976608b12c407a980e751bddb46fa
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-15 12:52:01 +0200

    Add MapService

--- interfaces/overview/src/app/pages/map/map.page.ts
+++ interfaces/overview/src/app/pages/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from 'src/app/services/map.service';
 
 @Component({
   selector: 'app-map',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  constructor() {}
+  constructor(mapService: MapService) {}
 
   ngOnInit() {}
 }
