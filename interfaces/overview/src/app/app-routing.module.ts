fab46f242de8fcd798487c4972dde6293c11ce38
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-18 16:18:59 +0200

    Add documentation/testing pages

--- interfaces/overview/src/app/app-routing.module.ts
+++ interfaces/overview/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
