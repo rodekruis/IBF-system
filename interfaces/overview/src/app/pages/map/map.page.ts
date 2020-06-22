8765d6fbe30fa374b1641f375e1b6fbfd2718560
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-22 12:03:11 +0200

    Add map-instance for development/testing + fix tests

--- interfaces/overview/src/app/pages/map/map.page.ts
+++ interfaces/overview/src/app/pages/map/map.page.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MapService } from 'src/app/services/map.service';
 
 @Component({
-  selector: 'app-map',
+  selector: 'app-map-page',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  constructor(mapService: MapService) {}
+  constructor(public mapService: MapService) {}
 
   ngOnInit() {}
+
+  public toggleFirstLayer() {
+    const newState = !this.mapService.state.layers[0].active;
+    this.mapService.state.layers[0].active = newState;
+  }
 }
