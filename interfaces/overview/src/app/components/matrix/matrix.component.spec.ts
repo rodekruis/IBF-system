bd0a1e9a33c1707e733c79b48832cb65f5ea8fba
Elwin Schmitz <eschmitz@redcross.nl>
2020-07-02 09:52:06 +0200

    Fix/Cleanup tests, use proper mocks+testing-client

--- interfaces/overview/src/app/components/matrix/matrix.component.spec.ts
+++ interfaces/overview/src/app/components/matrix/matrix.component.spec.ts
@@ -1,16 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { MapService } from 'src/app/services/map.service';
 import { MatrixComponent } from './matrix.component';
 
 describe('MatrixComponent', () => {
   let component: MatrixComponent;
   let fixture: ComponentFixture<MatrixComponent>;
 
+  const mockMapService = jasmine.createSpyObj('MapService', ['state']);
+  mockMapService.state.and.returnValue({
+    layers: [],
+  });
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [MatrixComponent],
-      imports: [IonicModule.forRoot(), HttpClientModule],
+      imports: [IonicModule.forRoot()],
+      providers: [{ provide: MapService, useValue: mockMapService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MatrixComponent);
--- interfaces/overview/src/app/services/map.service.spec.ts
+++ interfaces/overview/src/app/services/map.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { MapService } from './map.service';
 
@@ -7,7 +7,7 @@ describe('MapService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(MapService);
   });
