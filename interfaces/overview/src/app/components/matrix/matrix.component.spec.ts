7a1e5f95926d2d0b5f8cf1dcf4ec28d8fa8e70cf
Elwin Schmitz <eschmitz@redcross.nl>
2020-07-13 09:11:04 +0200

    Fix tests, use real MapService instead of mock
    As the service will use mock-data/requests during tests

--- interfaces/overview/src/app/components/matrix/matrix.component.spec.ts
+++ interfaces/overview/src/app/components/matrix/matrix.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { MapService } from 'src/app/services/map.service';
@@ -7,16 +8,11 @@ describe('MatrixComponent', () => {
   let component: MatrixComponent;
   let fixture: ComponentFixture<MatrixComponent>;
 
-  const mockMapService = jasmine.createSpyObj('MapService', ['state']);
-  mockMapService.state.and.returnValue({
-    layers: [],
-  });
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [MatrixComponent],
-      imports: [IonicModule.forRoot()],
-      providers: [{ provide: MapService, useValue: mockMapService }],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule],
+      providers: [{ provide: MapService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MatrixComponent);
