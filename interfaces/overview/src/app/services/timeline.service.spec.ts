03583646ddca778417198f99bca4ce67b279b230
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-07-29 01:35:35 +0200

    fix: test spec for timeline service

--- interfaces/overview/src/app/services/timeline.service.spec.ts
+++ interfaces/overview/src/app/services/timeline.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { TimelineService } from './timeline.service';
 
@@ -5,7 +6,9 @@ describe('TimelineService', () => {
   let service: TimelineService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(TimelineService);
   });
 
