e981d6edacf5644b5a491da44b2b703e089198d5
pphung <45565029+p-phung@users.noreply.github.com>
2020-08-27 01:05:56 +0200

    edit columns in df_trigger

--- services/IBF-pipeline/pipeline/settings.py
+++ services/IBF-pipeline/pipeline/settings.py
@@ -11,7 +11,7 @@ CALCULATE_EXPOSURE = False
 
 # Use dummy-data and/or overwrite real data
 GLOFAS_DUMMY = False
-RAINFALL_DUMMY = False ## Phuoc's Egypt test
+RAINFALL_DUMMY = True
 OVERWRITE_DUMMY = True
 
 
