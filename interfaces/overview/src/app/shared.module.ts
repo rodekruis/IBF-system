5a5bb2052d39747a8e6cca4e4d406c47b9b94383
Jannis Visser <jannisvisser@redcross.nl>
2020-08-31 16:05:47 +0200

    AB#3064 lint

--- interfaces/overview/src/app/shared.module.ts
+++ interfaces/overview/src/app/shared.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { IonicModule } from '@ionic/angular';
 import { AggregatesComponent } from './components/aggregates/aggregates.component';
+import { ChatComponent } from './components/chat/chat.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { MapComponent } from './components/map/map.component';
 import { MatrixComponent } from './components/matrix/matrix.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
-import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, LeafletModule],
