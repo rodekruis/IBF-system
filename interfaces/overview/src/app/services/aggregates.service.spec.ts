98f50775b75584941cb024c4be7fb34c3d485f71
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-08-31 12:14:26 +0200

    AB#3064 fix: test spec errors

--- interfaces/overview/src/app/services/aggregates.service.spec.ts
+++ interfaces/overview/src/app/services/aggregates.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { AggregatesService } from './aggregates.service';
 
@@ -5,7 +6,9 @@ describe('AggregatesService', () => {
   let service: AggregatesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(AggregatesService);
   });
 
