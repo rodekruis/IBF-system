ffe4342cca13426ec22ed26470f1a85fd5696dee
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-08-31 12:09:26 +0200

    AB#3064 fix: lint errors

--- interfaces/overview/src/app/components/aggregates/aggregates.component.spec.ts
+++ interfaces/overview/src/app/components/aggregates/aggregates.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { AggregatesComponent } from './aggregates.component';
 
 describe('AggregatesComponent', () => {
@@ -9,8 +8,8 @@ describe('AggregatesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AggregatesComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [AggregatesComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AggregatesComponent);
