31fe03322e28e22ea85688bc78289f9b60fc6b28
Jannis Visser <jannisvisser@redcross.nl>
2020-08-31 16:05:47 +0200

    AB#3064 lint

--- interfaces/overview/src/app/components/chat/chat.component.spec.ts
+++ interfaces/overview/src/app/components/chat/chat.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ChatComponent } from './chat.component';
 
 describe('ChatComponent', () => {
@@ -9,8 +8,8 @@ describe('ChatComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ChatComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ChatComponent);
--- interfaces/overview/src/app/components/chat/chat.component.ts
+++ interfaces/overview/src/app/components/chat/chat.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
