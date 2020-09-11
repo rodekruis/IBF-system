e2799e8dbc416a807df794239c2778538e325ae6
Jannis Visser <jannisvisser@redcross.nl>
2020-09-11 11:25:05 +0200

    AB#3334 fix webhook

--- tools/webhook.js
+++ tools/webhook.js
@@ -2,6 +2,8 @@ const http = require("http");
 const crypto = require("crypto");
 const child_process = require("child_process");
 const exec = child_process.exec;
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/', secret: process.env.GITHUB_WEBHOOK_SECRET })
 
 // ----------------------------------------------------------------------------
 //   Functions/Methods/etc:
@@ -30,58 +32,22 @@ function deploy(target) {
 //   Webhook Service:
 // ----------------------------------------------------------------------------
 
-http
-  .createServer(function(req, res) {
-    let body = [];
-    req.on("data", function(chunk) {
-      body.push(chunk);
-    });
-    req.on("end", function() {
-      let str = Buffer.concat(body).toString();
-      let sig =
-        "sha1=" +
-        crypto
-          .createHmac("sha1", process.env.GITHUB_WEBHOOK_SECRET)
-          .update(str)
-          .digest("hex");
-      let payload = JSON.parse(str);
-
-      if (
-        req.headers["x-hub-signature"] !== sig ||
-        (
-          payload.pull_request &&
-          payload.pull_request.merged &&
-          payload.pull_request.title.includes("[SKIP CD]")
-        )
-      ) {
-        return
-      }
-
-      if (
-        process.env.NODE_ENV === "test" &&
-        payload.action === "closed" &&
-        payload.pull_request.merged
-      ) {
-        deploy()
-        return
-      }
-
-      if (
-        (
-          process.env.NODE_ENV === "production" ||
-          process.env.NODE_ENV === "staging"
-        ) &&
-        payload.action === "released" &&
-        payload.release.draft === false &&
-        payload.release.target_commitish &&
-        payload.release.target_commitish.includes(process.env.VERSION)
-      ) {
-        deploy(payload.release.target_commitish);
-        return
-      }
-    });
-    res.end();
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
   })
-  .listen(process.env.NODE_PORT);
+}).listen(process.env.NODE_PORT)
+
+handler.on('pull_request', function (event) {
+  if (
+    process.env.NODE_ENV === "test" &&
+    event.payload.action === "closed" &&
+    event.payload.pull_request.merged
+  ) {
+    deploy()
+    return
+  }
+})
 
 console.log(`Listening on port ${process.env.NODE_PORT}`);
