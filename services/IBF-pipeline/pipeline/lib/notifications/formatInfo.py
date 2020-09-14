858fb667c4a6726b78a469a64077b32116217445
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-09-14 13:21:29 +0200

    AB#3358 feat: fill email values with country appropriate links

--- services/IBF-pipeline/pipeline/lib/notifications/formatInfo.py
+++ services/IBF-pipeline/pipeline/lib/notifications/formatInfo.py
@@ -4,20 +4,31 @@ from datetime import date
 
 def formatInfo(info, countryCode):
 
-    htmlToday = str(date.today())
+    today = str(date.today())
 
     if countryCode == 'UGA':
-        htmlLogo = 'https://mcusercontent.com/e71f3b134823403aa6fe0c6dc/images/aca24765-1e55-44ce-b534-aa9e1b596a40.jpg'
+        logo = 'https://mcusercontent.com/e71f3b134823403aa6fe0c6dc/images/aca24765-1e55-44ce-b534-aa9e1b596a40.jpg'
+        triggerStatement = 'URCS will activate this EAP when GloFAS issues a forecast of at least <b>60% probability</b> (based on the different ensemble runs) <b>of a 5-year return period</b> flood occurring in flood prone districts, which will be anticipated to affect <b>more than 1,000hh</b>. The EAP will be triggered with a <b>lead time of 7 days</b> and a FAR of <b>not more than 0.5.</b>'
+        linkDashboard = 'http://ibf-system.westeurope.cloudapp.azure.com/overview'
+        linkEAPSOP = 'https://rodekruis.sharepoint.com/sites/510-CRAVK-510/_layouts/15/Doc.aspx?OR=teams&action=edit&sourcedoc={0FFAA5EF-423C-4F81-A51E-BEA98D06E91C}'
+        linkWhatsApp = 'https://web.whatsapp.com/'
     elif countryCode == 'ZMB':
-        htmlLogo = 'https://mcusercontent.com/acdf2ad027e3073babce9067d/images/2d20901d-901d-4678-b32a-8facdb16bd82.png'
+        logo = 'https://mcusercontent.com/acdf2ad027e3073babce9067d/images/2d20901d-901d-4678-b32a-8facdb16bd82.png'
+        triggerStatement = ''
+        linkDashboard = 'http://ibf-system.westeurope.cloudapp.azure.com/overview'
+        linkEAPSOP = 'https://docs.google.com/document/d/18SG6UklAYsY5EkVAINnZUH6D_tvry3Jh479mpVTehRU/edit?ts=5da1dba5#heading=h.gjdgxs'
+        linkWhatsApp = 'https://web.whatsapp.com/'
     else:
-        htmlLogo = 'https://mcusercontent.com/acdf2ad027e3073babce9067d/images/2d20901d-901d-4678-b32a-8facdb16bd82.png'
+        logo = 'https://mcusercontent.com/acdf2ad027e3073babce9067d/images/2d20901d-901d-4678-b32a-8facdb16bd82.png'
+        triggerStatement = ''
+        linkDashboard = 'http://ibf-system.westeurope.cloudapp.azure.com/overview'
+        linkEAPSOP = 'https://google.com/'
+        linkWhatsApp = 'https://web.whatsapp.com/'
 
-    htmlLeadTime = ''
-    # affectedPopString = ": Estimate of exposed population: "
+    leadTime = ''
     stringList = []
     totalPopAffected3day = 0
-    htmlList3 = \
+    table3Day = \
         '<div> \
             <strong>Forecast 3 days from today:</strong> \
         </div> \
@@ -33,20 +44,20 @@ def formatInfo(info, countryCode):
     subject3day = ''
     for districtInfo in info['data']:
         if districtInfo[2] == '3-day':
-            htmlLeadTime = '3 days from today'
+            leadTime = '3 days from today'
             affectedPopStr = str("{0:,.0f}".format(round(districtInfo[1])))
             stringDistrict = districtInfo[0]
             stringList.append(stringDistrict)
-            htmlList3 += '<tr><td>' + districtInfo[0] + '</td><td>' + affectedPopStr + '</td><td>' + districtInfo[3] + '</td></tr>'
+            table3Day += '<tr><td>' + districtInfo[0] + '</td><td>' + affectedPopStr + '</td><td>' + districtInfo[3] + '</td></tr>'
             totalPopAffected3day = totalPopAffected3day + districtInfo[1]
             subject3day = "Estimate of exposed population: " + str("{0:,.0f}".format(round(totalPopAffected3day))) + " (3-day). "
-    htmlList3 += '</tbody></table>'
+    table3Day += '</tbody></table>'
     if stringList == []:
-        htmlList3 = ''
+        table3Day = ''
 
     totalPopAffected7day = 0
     stringList = []
-    htmlList7 = \
+    table7Day = \
         '<div> \
             <strong>Forecast 7 days from today:</strong> \
         </div> \
@@ -62,57 +73,44 @@ def formatInfo(info, countryCode):
     subject7day = ''
     for districtInfo in info['data']:
         if districtInfo[2] == '7-day':
-            if htmlLeadTime == '3 days from today':
-                htmlLeadTime = '3 and 7 days from today'
+            if leadTime == '3 days from today':
+                leadTime = '3 and 7 days from today'
             else:
-                htmlLeadTime = '7 days from today'
+                leadTime = '7 days from today'
             affectedPopStr = str("{0:,.0f}".format(round(districtInfo[1])))
             stringDistrict = districtInfo[0]
             stringList.append(stringDistrict)
-            htmlList7 += '<tr><td>' + districtInfo[0] + '</td><td>' + affectedPopStr + '</td><td>' + districtInfo[3] + '</td></tr>'
-            totalPopAffected7day = totalPopAffected7day + districtInfo[1]            
+            table7Day += '<tr><td>' + districtInfo[0] + '</td><td>' + affectedPopStr + '</td><td>' + districtInfo[3] + '</td></tr>'
+            totalPopAffected7day = totalPopAffected7day + districtInfo[1]
             subject7day = "Estimate of exposed population: " + str("{0:,.0f}".format(round(totalPopAffected7day))) + " (7-day). "
-    htmlList7 += '</tbody></table>'
+    table7Day += '</tbody></table>'
     if stringList == []:
-        htmlList7 = ''
+        table7Day = ''
 
-    
     file = codecs.open("lib/notifications/flood-trigger-notification.html", "r")
-    theHtml = file.read()
+    htmlTemplate = file.read()
 
-    replacablePartToday = '(TODAY)'
-    replacablePartLeadTime = '(LEAD-DATE)'
-    replacablePartLogo = '(IMG-LOGO)'
-    replacablePart3day = '(TABLE-3-DAYS)'
-    replacablePart7day = '(TABLE-7-DAYS)'
+    placeholderToday = '(TODAY)'
+    placeholderLeadTime = '(LEAD-DATE)'
+    placeholderLogo = '(IMG-LOGO)'
+    placeholderTable3Day = '(TABLE-3-DAYS)'
+    placeholderTable7Day = '(TABLE-7-DAYS)'
+    placeholderTriggerStatement = '(TRIGGER-STATEMENT)'
+    placeholderLinkDashboard = '(LINK-DASHBOARD)'
+    placeholderLinkEAPSOP = '(LINK-EAP-SOP)'
+    placeholderLinkWhatsApp = '(LINK-WHATSAPP)'
 
-    
-    # if totalPopAffected3day>0 or totalPopAffected7day>0:
-    #     htmlWhatsapp="<br>You can join a WhatsApp group to communicate with others about the potential flood by clicking this link on your phone: https://chat.whatsapp.com/FH2hls1iOeNJF844L4cOjr"
-    #     htmlGlofas="<br><br>* The alert class is based on the GLOFAS probability. GLOFAS produces 51 forecasts runs per stations daily. This probability is the percentage of these runs that exceeds the trigger level. Respective levels of 60%, 70% and 80% have been chosen as minimum, medium and maximum threshold."
-    # else:
-    #     htmlWhatsapp=""
-    #     htmlGlofas=""
-    # if totalPopAffected3day>0:
-    #     htmlGlofasProb3="(Alert class* in brackets)"
-    # else:
-    #     htmlGlofasProb3=""
-    # if totalPopAffected7day>0:
-    #     htmlGlofasProb7="(Alert class* in brackets)"
-    # else:
-    #     htmlGlofasProb7=""
+    htmlEmail = htmlTemplate.replace(placeholderToday, today) \
+                    .replace(placeholderLeadTime, leadTime) \
+                    .replace(placeholderLogo, logo) \
+                    .replace(placeholderTable3Day, table3Day) \
+                    .replace(placeholderTable7Day, table7Day) \
+                    .replace(placeholderTriggerStatement, triggerStatement) \
+                    .replace(placeholderLinkDashboard, linkDashboard) \
+                    .replace(placeholderLinkEAPSOP, linkEAPSOP) \
+                    .replace(placeholderLinkWhatsApp, linkWhatsApp)
 
-    newHtml = theHtml.replace(replacablePartToday, htmlToday) \
-                    .replace(replacablePartLeadTime, htmlLeadTime) \
-                    .replace(replacablePartLogo, htmlLogo) \
-                    .replace(replacablePart3day, htmlList3) \
-                    .replace(replacablePart7day, htmlList7)
-                    # .replace(replacablePartWhatsapp, htmlWhatsapp) \
-                    # .replace(replacablePartGlofas, htmlGlofas) \
-                    # .replace(replacablePartGlofas3, htmlGlofasProb3) \
-                    # .replace(replacablePartGlofas7, htmlGlofasProb7)
+    subject = "Flood Warning: " + subject3day + subject7day
 
-    subject = "Flood warning: " + subject3day + subject7day
-
-    emailContent = {"subject": subject, "html": newHtml}
+    emailContent = {"subject": subject, "html": htmlEmail}
     return emailContent
