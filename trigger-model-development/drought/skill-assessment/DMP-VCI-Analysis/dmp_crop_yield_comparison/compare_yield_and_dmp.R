04a6fdea1eba9c563875c0878b03d5d95d457e64
boukepieter <boukepieter@gmail.com>
2020-10-06 15:38:01 +0200

    update

--- trigger-model-development/drought/skill-assessment/DMP-VCI-Analysis/dmp_crop_yield_comparison/compare_yield_and_dmp.R
+++ trigger-model-development/drought/skill-assessment/DMP-VCI-Analysis/dmp_crop_yield_comparison/compare_yield_and_dmp.R
@@ -19,6 +19,7 @@ dmp <- dmp %>%
 
 # for wheat 07,08,09,10
 # for maize mid-march sept 03,04,05,06,07,08,09
+# april to july for ZWE
 
 dmp2 <- dmp  %>%
   left_join(dmp %>% filter(month %in% c('07','08','09','10') ) %>% group_by(pcode,year) %>%
@@ -45,19 +46,18 @@ dat <- yield_dmp %>% gather(var, val, -pcode,-year,-Dates,-date,-month, -class,-
 ggplot(dat, aes(x =  year, y=val, colour = var)) +
   geom_point() +facet_wrap( ~ lznameen)
 
-Yield_DMP2<- Yield_DMP %>% # group_by(pcode) %>% mutate(cor_coef_lz = cor(yield, dmp))%>% ungroup() %>% as.data.frame()%>%
-  group_by(LZTYPE) %>% mutate(cor_wheat_lzt = cor(yield, dmp_wheat_m),cor_maize_lzt = cor(yield, dmp_maize_m))%>% ungroup() %>% as.data.frame()%>%
-  group_by(LZNAMEEN) %>% mutate(cor_wheat_lzn =cor(yield, dmp_wheat_m),cor_maize_lzn = cor(yield, dmp_maize_m))%>% ungroup() %>% as.data.frame()%>%
-  group_by(County) %>% mutate(cor_wheat_ad =cor(yield, dmp_wheat_m),cor_maize_lad = cor(yield, dmp_maize_m))%>% ungroup() %>% as.data.frame()
+yield_dmp2<- yield_dmp %>% # group_by(pcode) %>% mutate(cor_coef_lz = cor(yield, dmp))%>% ungroup() %>% as.data.frame()%>%
+  group_by(class) %>% mutate(cor_wheat_lzt = cor(yield, dmp_wheat_m),cor_maize_lzt = cor(yield, dmp_maize_m))%>% ungroup() %>% as.data.frame()%>%
+  group_by(lznameen) %>% mutate(cor_wheat_lzn = cor(yield, dmp_wheat_m),cor_maize_lzn = cor(yield, dmp_maize_m))%>% ungroup() %>% as.data.frame()
 
-dat<- Yield_DMP2 %>% select(pcode,year,County,LZTYPE ,LZNAMEEN,cor_wheat_lzt, cor_maize_lzt, cor_wheat_lzn, cor_maize_lzn) %>%
-  gather(var, val, -pcode,-year,-County,-LZTYPE,-LZNAMEEN)
+dat<- yield_dmp2 %>% select(pcode,year,class,lznameen,cor_wheat_lzt, cor_maize_lzt, cor_wheat_lzn, cor_maize_lzn) %>%
+  gather(var, val, -pcode,-year,-class,-lznameen)
 
-ggplot(dat, aes(x =  County, y=val, colour = var)) +
-  geom_point() +facet_wrap( ~ LZTYPE)
+ggplot(dat, aes(x = lznameen, y=val, colour = var)) +
+  geom_point() + facet_wrap( ~ class)
 
-ggplot(dat, aes(x=LZTYPE, y=val)) +
-  geom_boxplot(outlier.colour="red") +facet_wrap( ~ var) + #theme(axis.text.x = element_text(angle = 90))
+ggplot(dat, aes(x = class, y=val)) +
+  geom_boxplot(outlier.colour="red") + facet_wrap( ~ var) + #theme(axis.text.x = element_text(angle = 90))
   scale_x_discrete(name="Livelyhood Zone") +
   scale_y_continuous(name="Correlation Coeff", limits=c(0, 1))+ ggtitle("Correlation between Crop Yield and DMP")+
   theme(axis.text.x = element_text(face = "bold", color = "#993333",
@@ -65,11 +65,11 @@ ggplot(dat, aes(x=LZTYPE, y=val)) +
       axis.text.y = element_text(face = "bold", color = "blue",
                                  size = 14))
 
-Yield_DMP3<-Yield_DMP2 %>% distinct(pcode, .keep_all = TRUE) %>%
+yield_dmp3 <- yield_dmp2 %>% distinct(pcode, .keep_all = TRUE) %>%
   select(pcode,dmp_wheat_m,dmp_maize_m,cor_wheat_lzt,cor_maize_lzt,cor_wheat_lzn,cor_maize_lzn,cor_wheat_ad,cor_maize_lad)%>%
   mutate(LZCODE=pcode)
 
-Eth_LZH2<- Eth_LZH %>%left_join(Yield_DMP3,by='LZCODE')
+lhz2<- lhz %>% left_join(yield_dmp3,by='LZCODE')
 
 
 
