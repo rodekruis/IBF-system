fffdcb9dd55d8f0d0350242c58a6d542ae3cc9e8
Jannis Visser <jannisvisser@redcross.nl>
2020-10-30 10:51:49 +0100

    AB#4182 no mock-scenarios for zambia

--- interfaces/IBF-dashboard/src/app/mocks/mock-scenario-service/mock-scenario.service.ts
+++ interfaces/IBF-dashboard/src/app/mocks/mock-scenario-service/mock-scenario.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { MockScenario } from 'src/app/mocks/mock-scenario.enum';
+import { Country } from 'src/app/models/country.model';
 import { AggregatesService } from 'src/app/services/aggregates.service';
+import { CountryService } from 'src/app/services/country.service';
 import { EapActionsService } from 'src/app/services/eap-actions.service';
 import { EventService } from 'src/app/services/event.service';
 import { MapService } from 'src/app/services/map.service';
@@ -11,13 +14,8 @@ import { TimelineService } from 'src/app/services/timeline.service';
 })
 export class MockScenarioService {
   public mockScenario: MockScenario = MockScenario.real;
-  public mockScenarios: MockScenario[] = [
-    MockScenario.real,
-    MockScenario.noEvent,
-    MockScenario.newEvent,
-    MockScenario.existingEvent,
-    MockScenario.oldEvent,
-  ];
+  public mockScenarios: MockScenario[];
+  private countrySubscription: Subscription;
 
   constructor(
     private eapActionsService: EapActionsService,
@@ -25,7 +23,28 @@ export class MockScenarioService {
     private mapService: MapService,
     private eventService: EventService,
     public aggregatesService: AggregatesService,
-  ) {}
+    private countryService: CountryService,
+  ) {
+    this.countrySubscription = this.countryService
+      .getCountrySubscription()
+      .subscribe(async (country: Country) => {
+        this.setAvailableMockScenarios(country);
+      });
+  }
+
+  private setAvailableMockScenarios(country: Country) {
+    if (country.countryCode === 'UGA') {
+      this.mockScenarios = [
+        MockScenario.real,
+        MockScenario.noEvent,
+        MockScenario.newEvent,
+        MockScenario.existingEvent,
+        MockScenario.oldEvent,
+      ];
+    } else {
+      this.mockScenarios = [MockScenario.real];
+    }
+  }
 
   public setMockScenario(mockScenario: MockScenario) {
     this.mockScenario = mockScenario;
