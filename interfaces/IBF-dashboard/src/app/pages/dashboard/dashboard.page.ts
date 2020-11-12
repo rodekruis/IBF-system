ef8eb0d7e7914fa897a263c148042bb2006d5a3e
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-11-12 15:09:03 +0100

    AB#4356 build: show version on interface

--- interfaces/IBF-dashboard/src/app/pages/dashboard/dashboard.page.ts
+++ interfaces/IBF-dashboard/src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,7 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage {
+  public version: string = environment.ibf_system_version;
   public isDev = false;
   private readonly adminRole = 'admin';
 
