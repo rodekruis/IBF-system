66a76dc5ec46f83942fc942093395e29149f4a4d
Jannis Visser <jannisvisser@redcross.nl>
2020-11-23 09:12:22 +0100

    AB#4733 separate mailchimp by country

--- services/IBF-pipeline/pipeline/lib/notifications/sendNotification.py
+++ services/IBF-pipeline/pipeline/lib/notifications/sendNotification.py
@@ -1,15 +1,16 @@
 from mailchimp3 import MailChimp
 import datetime
-from secrets import LIST_ID
+from secrets import LIST_ID, SEGMENT
 from settings import FROM_EMAIL, FROM_EMAIL_NAME
 
 
 
 class EmailClient(MailChimp):
  
-    def sendNotification(self, emailContent): 
+    def sendNotification(self, emailContent, countryCode):
+        self.countryCode = countryCode 
         title = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        campaign_data = self.populateCampaginData(LIST_ID, title, emailContent["subject"], "Flood")
+        campaign_data = self.populateCampaignData(LIST_ID, title, emailContent["subject"], "Flood")
         campaign = self.createCampaign(campaign_data)
         self.populateContent(campaign["id"], emailContent["html"])
         self.campaigns.actions.send(campaign["id"])
@@ -21,7 +22,6 @@ class EmailClient(MailChimp):
 
     
     def populateContent(self, campaign_id, body):
-
         data = { 
             "html" : body
         }
@@ -29,14 +29,22 @@ class EmailClient(MailChimp):
         return result
 
 
-    def populateCampaginData(self, list_id, title, subject, data, auto_tweet=False):
-        campaign_data = {"settings": {"title": title,
-                                    "subject_line": subject,
-                                    "from_name": FROM_EMAIL_NAME,
-                                    "reply_to": FROM_EMAIL,
-                                    "data": data,
-                                    "auto_tweet": auto_tweet
-                                    },
-                        "recipients": {"list_id": list_id},
-                        "type": "regular"}
+    def populateCampaignData(self, list_id, title, subject, data, auto_tweet=False):
+        campaign_data = {
+            "settings": {
+                "title": title,
+                "subject_line": subject,
+                "from_name": FROM_EMAIL_NAME,
+                "reply_to": FROM_EMAIL,
+                "data": data,
+                "auto_tweet": auto_tweet
+            },
+            "recipients": {
+                "list_id": list_id,
+                "segment_opts": {
+                    "saved_segment_id": SEGMENT[self.countryCode]
+                }
+            },
+            "type": "regular"
+        }
         return campaign_data
