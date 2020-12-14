451feb51710cc50e66cec5991114bc240d336264
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-12-14 17:33:22 +0100

    feat: remove secrets from git to avoid adding user information AB#5153

--- services/API-service/src/example.secrets.ts
+++ services/API-service/src/example.secrets.ts
@@ -1,5 +1,96 @@
-export const SECRET = 'secret-key';
-export const USERCONFIG = {
-  password: 'password',
-  username: 'username',
-};
+import { CountryStatus } from './country/country-status.enum';
+import { UserRole } from './user/user-role.enum';
+import { UserStatus } from './user/user-status.enum';
+
+export const SECRET = process.env.SECRET;
+
+export const USERCONFIGS = [
+  {
+    email: 'dunant@redcross.nl',
+    username: 'dunant',
+    firstName: 'Henry',
+    lastName: 'Dunant',
+    role: UserRole.Admin,
+    password: 'password',
+    status: UserStatus.Active,
+    countries: ['UGA', 'ZMB', 'KEN', 'ETH'],
+  },
+  {
+    email: 'uganda@redcross.nl',
+    username: 'uganda',
+    firstName: 'Uganda',
+    lastName: 'Manager',
+    role: UserRole.DisasterManager,
+    password: 'password',
+    status: UserStatus.Active,
+    countries: ['UGA'],
+  },
+  {
+    email: 'zambia@redcross.nl',
+    username: 'zambia',
+    firstName: 'Zambia',
+    lastName: 'Manager',
+    role: UserRole.DisasterManager,
+    password: 'password',
+    status: UserStatus.Active,
+    countries: ['ZMB'],
+  },
+  {
+    email: 'kenya@redcross.nl',
+    username: 'kenya',
+    firstName: 'Kenya',
+    lastName: 'Manager',
+    role: UserRole.DisasterManager,
+    password: 'password',
+    status: UserStatus.Active,
+    countries: ['KEN'],
+  },
+  {
+    email: 'ethiopia@redcross.nl',
+    username: 'ethiopia',
+    firstName: 'Ethiopia',
+    lastName: 'Manager',
+    role: UserRole.DisasterManager,
+    password: 'password',
+    status: UserStatus.Active,
+    countries: ['ETH'],
+  },
+  // {
+  //   email: 'egypt@redcross.nl',
+  //   username: 'egypt',
+  //   firstName: 'Egypt',
+  //   lastName: 'Manager',
+  //   role: UserRole.DisasterManager,
+  //   password: 'password',
+  //   status: UserStatus.Active,
+  //   countries: ['EGY'],
+  // },
+];
+
+export const COUNTRYCONFIGS = [
+  {
+    countryCode: 'UGA',
+    countryName: 'Uganda',
+    status: CountryStatus.Active,
+  },
+  {
+    countryCode: 'ZMB',
+    countryName: 'Zambia',
+    status: CountryStatus.Active,
+  },
+  {
+    countryCode: 'KEN',
+    countryName: 'Kenya',
+    status: CountryStatus.Active,
+  },
+  {
+    countryCode: 'ETH',
+    countryName: 'Ethiopia',
+    status: CountryStatus.Active,
+  },
+  {
+    countryCode: 'EGY',
+    countryName: 'Egypt',
+    status: CountryStatus.Active,
+  },
+];
