80eda9ad30d7e7a41c91c851f900b203b76c2544
Jannis Visser <jannisvisser@redcross.nl>
2021-01-11 14:55:42 +0100

    AB#5513 show agg-indicators based on metadata

--- interfaces/IBF-dashboard/src/app/types/indicator-group.ts
+++ interfaces/IBF-dashboard/src/app/types/indicator-group.ts
@@ -12,6 +12,7 @@ export class Indicator {
   active: boolean;
   numberFormatMap: NumberFormat;
   numberFormatAggregate: NumberFormat;
+  aggregateIndicator: boolean;
   group: IndicatorGroup;
 }
 
