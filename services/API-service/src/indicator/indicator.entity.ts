619e4f064dd7ffdc677f5846017a23f302274786
Jannis Visser <jannisvisser@redcross.nl>
2021-01-11 14:56:23 +0100

    AB#5509 switch ibf-metadata to seed-script

---
+++ services/API-service/src/indicator/indicator.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('indicator')
+export class IndicatorEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id: number;
+  @Column()
+  public country_code: string;
+  @Column()
+  public name: string;
+  @Column()
+  public label: string;
+  @Column()
+  public group: string;
+  @Column()
+  public icon: string;
+  @Column()
+  public weightedAvg: boolean;
+  @Column()
+  public active: boolean;
+  @Column()
+  public numberFormatMap: string;
+  @Column()
+  public aggregateIndicator: boolean | null;
+  @Column()
+  public numberFormatAggregate: string;
+  @Column()
+  public source: string;
+  @Column()
+  public description: string;
+}
