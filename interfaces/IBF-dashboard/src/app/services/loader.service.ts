2ceedb1e8f68f4ebc4cc46994f60dd840fbcdb75
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-01-16 15:14:54 +0100

    fix: loader issue AB#5261

--- interfaces/IBF-dashboard/src/app/services/loader.service.ts
+++ interfaces/IBF-dashboard/src/app/services/loader.service.ts
@@ -5,7 +5,8 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LoaderService {
-  private loaderSubject = new BehaviorSubject<boolean>(true);
+  private loaderSubject = new BehaviorSubject<boolean>(false);
+  private queue = [];
 
   constructor() {}
 
@@ -13,7 +14,21 @@ export class LoaderService {
     return this.loaderSubject.asObservable();
   }
 
-  setLoader(loading: boolean): void {
-    this.loaderSubject.next(loading);
+  updateQueue(item: string, done: boolean): void {
+    const queuePosition = this.queue.indexOf(item);
+    if (done) {
+      if (queuePosition >= 0) {
+        this.queue.splice(queuePosition, 1);
+      }
+    } else {
+      if (queuePosition < 0) {
+        this.queue.push(item);
+      }
+    }
+  }
+
+  setLoader(item: string, done: boolean): void {
+    this.updateQueue(item, done);
+    this.loaderSubject.next(this.queue.length > 0);
   }
 }
