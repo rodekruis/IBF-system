2b9ee46141abfaff4b28ad9b6af4c9c1ca37c39b
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-02-01 11:15:40 +0100

    fix: filter countries by user on load

--- interfaces/IBF-dashboard/src/app/components/country-switcher/country-switcher.component.ts
+++ interfaces/IBF-dashboard/src/app/components/country-switcher/country-switcher.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from 'src/app/auth/auth.service';
 import { CountryService } from 'src/app/services/country.service';
 
 @Component({
@@ -8,14 +7,7 @@ import { CountryService } from 'src/app/services/country.service';
   styleUrls: ['./country-switcher.component.scss'],
 })
 export class CountrySwitcherComponent {
-  constructor(
-    public countryService: CountryService,
-    private authService: AuthService,
-  ) {
-    this.authService.authenticationState$.subscribe(
-      this.countryService.filterCountriesForUser,
-    );
-  }
+  constructor(public countryService: CountryService) {}
 
   public handleCountryChange($event) {
     this.countryService.selectCountry($event.detail.value);
