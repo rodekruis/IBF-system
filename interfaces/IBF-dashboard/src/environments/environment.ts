9037f18fba748a8fe4807f7d891fd8b4c6ec008c
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-02-08 16:53:13 +0100

    feat: configuration env var for interface and favicon for each environment

--- interfaces/IBF-dashboard/src/environments/environment.ts
+++ interfaces/IBF-dashboard/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  configuration: 'development',
 
   // API
   api_url: 'http://localhost:3000/api/',
