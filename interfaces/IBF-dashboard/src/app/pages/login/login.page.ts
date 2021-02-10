128b482f8b4fae42b57ceacecb2d47ba9ed6232b
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-02-10 14:11:55 +0100

    feat: analytics page view events

--- interfaces/IBF-dashboard/src/app/pages/login/login.page.ts
+++ interfaces/IBF-dashboard/src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
+import { AnalyticsService } from 'src/app/analytics/analytics.service';
 import { VideoPopoverComponent } from 'src/app/components/video-popover/video-popover.component';
 import { environment } from 'src/environments/environment';
 
@@ -8,10 +9,17 @@ import { environment } from 'src/environments/environment';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
   public version: string = environment.ibfSystemVersion;
 
-  constructor(private popoverController: PopoverController) {}
+  constructor(
+    private popoverController: PopoverController,
+    private analyticsService: AnalyticsService,
+  ) {}
+
+  ngOnInit() {
+    this.analyticsService.logPageView('login');
+  }
 
   async presentPopover() {
     const popover = await this.popoverController.create({
