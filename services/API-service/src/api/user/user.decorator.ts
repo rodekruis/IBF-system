4c3f8a44ff7985cce22a222adec4a26339725ba0
jannisvisser <jannisvisser@redcross.nl>
2021-02-12 15:12:14 +0100

    fix: create + update user AB#6124

--- services/API-service/src/api/user/user.decorator.ts
+++ services/API-service/src/api/user/user.decorator.ts
@@ -13,8 +13,10 @@ export const UserDecorator = createParamDecorator(
     const token = req.headers.authorization
       ? (req.headers.authorization as string).split(' ')
       : null;
+    console.log('token: ', token);
     if (token && token[1]) {
       const decoded: User = jwt.verify(token[1], process.env.SECRET);
+
       return !!data ? decoded[data] : decoded;
     }
   },
