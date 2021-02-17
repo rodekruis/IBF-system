6e68ed84f5234ea5f1acad3b00db11d6d04235cb
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-02-17 16:30:53 +0100

    feat: analytics event for video guide button AB#5192

--- interfaces/IBF-dashboard/src/app/components/video-guide-button/video-guide-button.component.ts
+++ interfaces/IBF-dashboard/src/app/components/video-guide-button/video-guide-button.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
+import { AnalyticsEvent } from 'src/app/analytics/analytics.enum';
+import { AnalyticsService } from 'src/app/analytics/analytics.service';
+import { Country } from 'src/app/models/country.model';
+import { CountryService } from 'src/app/services/country.service';
+import { EventService } from 'src/app/services/event.service';
 import { environment } from 'src/environments/environment';
 import { VideoPopoverComponent } from '../video-popover/video-popover.component';
 
@@ -12,7 +17,12 @@ export class VideoGuideButtonComponent {
   @Input()
   public color: string = 'ibf-royal-blue';
 
-  constructor(private popoverController: PopoverController) {}
+  constructor(
+    private popoverController: PopoverController,
+    private analyticsService: AnalyticsService,
+    private countryService: CountryService,
+    private eventService: EventService,
+  ) {}
 
   async presentPopover() {
     const popover = await this.popoverController.create({
@@ -25,6 +35,16 @@ export class VideoGuideButtonComponent {
       translucent: true,
       showBackdrop: true,
     });
+
+    this.countryService
+      .getCountrySubscription()
+      .subscribe((country: Country) => {
+        this.analyticsService.logEvent(AnalyticsEvent.watchVideoGuide, {
+          country: country.countryCodeISO3,
+          isActiveEvent: this.eventService.state.activeEvent,
+          isActiveTrigger: this.eventService.state.activeTrigger,
+        });
+      });
 
     return await popover.present();
   }
