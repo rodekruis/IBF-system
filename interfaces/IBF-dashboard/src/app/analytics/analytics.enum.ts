08992662e839dfbb90f9f14f5132d170d6d83dde
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-02-17 16:47:31 +0100

    feat: analytics event for admin level button click AB#5192

--- interfaces/IBF-dashboard/src/app/analytics/analytics.enum.ts
+++ interfaces/IBF-dashboard/src/app/analytics/analytics.enum.ts
@@ -7,4 +7,5 @@ export enum AnalyticsEvent {
   watchVideoGuide = 'watch-video-guide',
   aboutTrigger = 'about-trigger',
   aggregateInformation = 'aggregate-information',
+  adminLevel = 'admin-level',
 }
