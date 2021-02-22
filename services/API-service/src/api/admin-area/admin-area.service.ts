e7e0cdd8850ffd9b0d135e11193b8c65223a4d95
jannisvisser <jannisvisser@redcross.nl>
2021-02-22 18:03:58 +0100

    fix: remove unused controllers/methods AB#6294

--- services/API-service/src/api/admin-area/admin-area.service.ts
+++ services/API-service/src/api/admin-area/admin-area.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { DataService } from '../data/data.service';
 import { AdminAreaEntity } from './admin-area.entity';
 
 @Injectable()
@@ -9,21 +8,5 @@ export class AdminAreaService {
   @InjectRepository(AdminAreaEntity)
   private readonly adminAreaRepository: Repository<AdminAreaEntity>;
 
-  public constructor(private readonly dataService: DataService) {}
-
-  public async getAdminAreasByCountry(countryCode): Promise<AdminAreaEntity[]> {
-    const adminAreas = await this.adminAreaRepository.find({});
-
-    const countryAdminAreas = adminAreas.filter(i =>
-      i.country_codes.split(',').includes(countryCode),
-    );
-
-    const event = await this.dataService.getEvent(countryCode);
-    const activeTrigger = event && !event.end_date;
-    countryAdminAreas.find(
-      (i): boolean => i.name === 'population_affected',
-    ).active = activeTrigger;
-
-    return countryAdminAreas;
-  }
+  public constructor() {}
 }
