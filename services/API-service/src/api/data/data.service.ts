5fa3e98bce31cc1acb4250673467c1d71eddda83
RubenGeo <rubenavalk@gmail.com>
2021-02-23 18:44:52 +0100

    feat: seperate .sql files AB#6297

--- services/API-service/src/api/data/data.service.ts
+++ services/API-service/src/api/data/data.service.ts
@@ -11,6 +11,7 @@ import {
   GlofasStation,
   RedCrossBranch,
 } from './geo.model';
+import fs from 'fs';
 
 @Injectable()
 export class DataService {
@@ -119,88 +120,19 @@ export class DataService {
   public async getTriggeredAreas(
     countryCode: string,
   ): Promise<TriggeredArea[]> {
-    const query = `select
-    *
-  from
-    (
-    select
-      e.pcode,
-      coalesce(a2.name, a1.name) as name,
-      coalesce(a2.population_affected, a1.population_affected) as population_affected,
-      e.id
-    from
-      "IBF-pipeline-output".event_pcode e
-    left join "IBF-API"."Admin_area_data2" a2 on
-      a2.pcode = e.pcode
-      and a2.country_code = $1
-      and a2.name is not null
-      and a2.current_prev = 'Current'
-    left join "IBF-API"."Admin_area_data1" a1 on
-      a1.pcode = e.pcode
-      and a1.current_prev = 'Current'
-      and a1.name is not null
-      and a1.country_code = $2
-    where
-      closed = false
-    group by
-      e.pcode,
-      a2.population_affected,
-      a1.population_affected,
-      a2.name,
-      a1.name,
-      e.id
-    order by
-      population_affected desc ) as ec
-  where
-    name is not null
-      `;
+    const query = fs
+      .readFileSync('./src/api/data/sql/get-triggered-areas.sql')
+      .toString();
 
     const result = await this.manager.query(query, [countryCode, countryCode]);
     return result;
   }
 
   public async getCountryEvent(countryCode: string): Promise<CountryEvent> {
-    const query = `
-    select
-      to_char(max(end_date) , 'yyyy-mm-dd') as end_date,
-      to_char(min(start_date), 'yyyy-mm-dd') as start_date,
-      max(country_code) as country_code,
-      max(active_trigger::int)::boolean as active_trigger
-    from
-      (
-      select
-        e.pcode,
-        coalesce(a2.country_code , a1.country_code) as country_code,
-        coalesce(a2.population_affected, a1.population_affected) as population_affected,
-        e.end_date,
-        e.start_date,
-        e.active_trigger
-      from
-        "IBF-pipeline-output".event_pcode e
-      left join "IBF-API"."Admin_area_data2" a2 on
-        a2.pcode = e.pcode
-        and a2.current_prev = 'Current'
-        and a2.country_code is not null
-      left join "IBF-API"."Admin_area_data1" a1 on
-        a1.pcode = e.pcode
-        and a1.current_prev = 'Current'
-        and a1.country_code is not null
-      where
-        closed = false
-      group by
-        e.pcode,
-        a2.population_affected,
-        a1.population_affected,
-        a2.country_code,
-        a1.country_code,
-        e.end_date,
-        e.start_date,
-        e."active_trigger"
-      order by
-        population_affected desc ) as event_pcode_country where country_code = $1
-          `;
+    const query = fs
+      .readFileSync('./src/api/data/sql/get-country-event.sql')
+      .toString();
     const result = await this.manager.query(query, [countryCode]);
-    console.log('result: ', result);
     if (!result[0].country_code) {
       return null;
     }
