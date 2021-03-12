4b3a777dc38a9d71b8e39ae0d53ab6afda59d06c
jannisvisser <jannisvisser@redcross.nl>
2021-03-12 16:39:45 +0100

    feat: automatically enable all countries for admin-user AB#6586

--- services/API-service/src/scripts/seed-init.ts
+++ services/API-service/src/scripts/seed-init.ts
@@ -116,11 +116,16 @@ export class SeedInit implements InterfaceScript {
           userEntity.firstName = user.firstName;
           userEntity.lastName = user.lastName;
           userEntity.userRole = user.userRole as UserRole;
-          userEntity.countries = await countryRepository.find({
-            where: user.countries.map((countryCodeISO3: string): object => {
-              return { countryCodeISO3: countryCodeISO3 };
-            }),
-          });
+          userEntity.countries =
+            user.userRole === UserRole.Admin
+              ? await countryRepository.find()
+              : await countryRepository.find({
+                  where: user.countries.map(
+                    (countryCodeISO3: string): object => {
+                      return { countryCodeISO3: countryCodeISO3 };
+                    },
+                  ),
+                });
           userEntity.userStatus = user.userStatus as UserStatus;
           userEntity.password = user.password;
           return userEntity;
