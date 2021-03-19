f057b3ab3ff3bfad9539e41ddbc7c8e21ceaeca0
RubenGeo <rubenavalk@gmail.com>
2021-03-19 14:26:09 +0100

    feat: cypress

--- prepare-test.sh
+++ prepare-test.sh
@@ -18,12 +18,11 @@ function migrate_database() {
     do
         echo "$SCHEMA"
         rm -f tools/db-dumps/ibf_$SCHEMA.dump
+        echo pg_dump -U $DB_USERNAME_TEST_VM -Fc -f tools/db-dumps/ibf_$SCHEMA.dump -h $DB_HOST_TEST_VM -n \"$SCHEMA\" geonode_datav3
         PGPASSWORD=$DB_PASSWORD_TEST_VM pg_dump -U $DB_USERNAME_TEST_VM -Fc -f tools/db-dumps/ibf_$SCHEMA.dump -h $DB_HOST_TEST_VM -n \"$SCHEMA\" geonode_datav3
-        PGPASSWORD=$DB_PASSWORD psql -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -p $DB_PORT_LOCAL -c 'drop schema "'$SCHEMA'" cascade; create schema "'$SCHEMA'";'
-        PGPASSWORD=$DB_PASSWORD pg_restore -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -p $DB_PORT_LOCAL --schema=$SCHEMA --clean tools/db-dumps/ibf_$SCHEMA.dump
+        PGPASSWORD=$DB_PASSWORD psql -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST_LOCAL -p $DB_PORT_LOCAL -c 'drop schema "'$SCHEMA'" cascade; create schema "'$SCHEMA'";'
+        PGPASSWORD=$DB_PASSWORD pg_restore -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST_LOCAL -p $DB_PORT_LOCAL --schema=$SCHEMA --clean tools/db-dumps/ibf_$SCHEMA.dump
     done
-
-    PGPASSWORD=$DB_PASSWORD psql -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -p $DB_PORT_LOCAL -f $SQL_FILE_PATH
 }
 
 prepare
