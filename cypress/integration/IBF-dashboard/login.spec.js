9d727851cfb6ed672ffe6e5a0b665ca446fdb382
RubenGeo <rubenavalk@gmail.com>
2021-03-23 11:26:05 +0100

    feat: cypress after merge

--- cypress/integration/IBF-dashboard/login.spec.js
+++ cypress/integration/IBF-dashboard/login.spec.js
@@ -1,6 +1,5 @@
 describe('Login Page', () => {
   beforeEach(() => {
-    // cy.setDashboard();
     cy.server();
   });
 
@@ -8,11 +7,15 @@ describe('Login Page', () => {
   it('lets the user log in', function () {
     cy.fixture('login').then((login) => {
       cy.visit(login.portal);
+
       cy.get('input[name="email"]').type(login.email);
       cy.get('input[name="password"]').type(login.password);
-
       cy.contains('Log In').click();
-      cy.get('[data-cy=logged-in-as]').contains('Logged In As:');
+
+      cy.get('[data-cy=logged-in-as]').contains('Logged In As:')
+      cy.url().should("include", "/", () => {
+        expect(localStorage.getItem("jwt")).to.exist()
+      })
     });
   });
 
