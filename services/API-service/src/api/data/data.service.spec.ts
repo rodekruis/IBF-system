f5b97047e289cfa39487910ce33450325692ae97
RubenGeo <rubenavalk@gmail.com>
2021-04-14 16:53:11 +0200

    fix: tests

--- services/API-service/src/api/data/data.service.spec.ts
+++ services/API-service/src/api/data/data.service.spec.ts
@@ -2,6 +2,7 @@ import { DataService } from './data.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserEntity } from '../user/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TriggerPerLeadTime } from '../upload/trigger-per-lead-time.entity';
 
 describe('User service', (): void => {
   let service: DataService;
@@ -11,7 +12,7 @@ describe('User service', (): void => {
       const module: TestingModule = await Test.createTestingModule({
         imports: [
           TypeOrmModule.forRoot(),
-          TypeOrmModule.forFeature([UserEntity]),
+          TypeOrmModule.forFeature([UserEntity, TriggerPerLeadTime]),
         ],
         providers: [DataService],
       }).compile();
--- services/API-service/src/api/upload/upload.service.spec.ts
+++ services/API-service/src/api/upload/upload.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { repositoryMockFactory } from '../../mock/repositoryMock.factory';
+import { CalculatedAffectedEntity } from './calculated-affected.entity';
+import { TriggerPerLeadTime } from './trigger-per-lead-time.entity';
 import { UploadService } from './upload.service';
 
 describe('UploadService', (): void => {
@@ -8,8 +12,18 @@ describe('UploadService', (): void => {
   beforeEach(
     async (): Promise<void> => {
       const module: TestingModule = await Test.createTestingModule({
-        imports: [TypeOrmModule.forRoot()],
-        providers: [UploadService],
+        providers: [
+          UploadService,
+          EntityManager,
+          {
+            provide: getRepositoryToken(TriggerPerLeadTime),
+            useFactory: repositoryMockFactory,
+          },
+          {
+            provide: getRepositoryToken(CalculatedAffectedEntity),
+            useFactory: repositoryMockFactory,
+          },
+        ],
       }).compile();
 
       service = module.get<UploadService>(UploadService);
