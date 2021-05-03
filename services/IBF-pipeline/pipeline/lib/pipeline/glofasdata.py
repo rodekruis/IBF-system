59924e42f702921963a4c4638e4a567711d4d86c
jannisvisser <jannisvisser@redcross.nl>
2021-05-03 16:22:43 +0200

    fix: don't try download if mock AB#7447

--- services/IBF-pipeline/pipeline/lib/pipeline/glofasdata.py
+++ services/IBF-pipeline/pipeline/lib/pipeline/glofasdata.py
@@ -42,12 +42,16 @@ class GlofasData:
         self.current_date = CURRENT_DATE.strftime('%Y-%m-%d')
 
     def process(self):
-        self.removeOldGlofasData()
-        self.download()
-        if self.country_code == 'ZMB': #Temporarily keep using FTP for Zambia
-            self.extractFtpData()
+        if SETTINGS_SECRET[self.country_code]['mock'] == False:
+            self.removeOldGlofasData()
+            self.download()
+        if SETTINGS_SECRET[self.country_code]['mock'] == True:
+            self.extractMockData()
         else:
-            self.extractApiData()
+            if self.country_code == 'ZMB': #Temporarily keep using FTP for Zambia
+                self.extractFtpData()
+            else:
+                self.extractApiData()
         self.findTrigger()
 
     def removeOldGlofasData(self):
@@ -97,7 +101,7 @@ class GlofasData:
         open(self.inputPath+'glofas-api-'+self.country_code+'-'+self.current_date+'.nc', 'wb').write(glofasDataFile.content)
 
     def extractFtpData(self):
-        print('\nExtracting FTP Glofas Data\n')
+        print('\nExtracting Glofas (FTP) Data\n')
 
         files = [f for f in listdir(self.inputPath) if isfile(
             join(self.inputPath, f)) and f.endswith('.nc')]
@@ -146,20 +150,6 @@ class GlofasData:
                         discharge = data['dis'].sel(
                             ensemble=ensemble, step=step).values[time][0]
 
-                        # DUMMY OVERWRITE DEPENDING ON COUNTRY SETTING
-                        if SETTINGS_SECRET[self.country_code]['mock'] == True:
-                            if SETTINGS_SECRET[self.country_code]['if_mock_trigger'] == True:
-                                if step < 5: # Only dummy trigger for 5-day and above
-                                    discharge = 0
-                                elif station['code'] == 'G1361':  # ZMB dummy flood station 1
-                                    discharge = 8000
-                                elif station['code'] == 'G1328':  # ZMB dummy flood station 2
-                                    discharge = 9000
-                                else:
-                                    discharge = 0
-                            else:
-                                discharge = 0
-
                         if discharge >= threshold:
                             count = count + 1
                         dis_sum = dis_sum + discharge
@@ -199,7 +189,7 @@ class GlofasData:
             print('Extracted Glofas data - Trigger per day File saved')
 
     def extractApiData(self):
-        print('\nExtracting Glofas Data\n')
+        print('\nExtracting Glofas (API) Data\n')
 
         # Load input data
         df_thresholds = pd.read_json(json.dumps(self.GLOFAS_STATIONS))
@@ -257,23 +247,100 @@ class GlofasData:
                         dischargeArray = ncData2['dis24'].sel(latitude=slice(st_lat+deltax,st_lat-deltax), longitude=slice(st_lon-deltax,st_lon+deltax),step=str(step)+' days',number=ensemble).values.flatten()
                         discharge = np.nanmax(dischargeArray)
 
+                        if discharge >= threshold:
+                            count = count + 1
+                        dis_sum = dis_sum + discharge
+
+                    prob = count/ensemble_options
+                    dis_avg = dis_sum/ensemble_options
+                    station['fc'] = dis_avg
+                    station['fc_prob'] = prob
+                    station['fc_trigger'] = 1 if prob > TRIGGER_LEVELS['minimum'] else 0
+
+                    if station['fc_trigger'] == 1:
+                        trigger_per_day[str(step)+'-day'] = True
+
+                    if step == self.leadTimeValue:
+                        stations.append(station)
+                    station = {}
+                    station['code'] = row['stationCode']
+
+
+        # Add 'no_station'
+        for station_code in ['no_station']:
+            station = {}
+            station['code'] = station_code
+            station['fc'] = 0
+            station['fc_prob'] = 0
+            station['fc_trigger'] = 0
+            stations.append(station)
+
+        with open(self.extractedGlofasPath, 'w') as fp:
+            json.dump(stations, fp)
+            print('Extracted Glofas data - File saved')
+
+        with open(self.triggerPerDay, 'w') as fp:
+            json.dump([trigger_per_day], fp)
+            print('Extracted Glofas data - Trigger per day File saved')
+
+    def extractMockData(self):
+        print('\nExtracting Glofas (mock) Data\n')
+
+        # Load input data
+        df_thresholds = pd.read_json(json.dumps(self.GLOFAS_STATIONS))
+        df_thresholds = df_thresholds.set_index("stationCode", drop=False)
+        df_district_mapping = pd.read_json(json.dumps(self.DISTRICT_MAPPING))
+        df_district_mapping = df_district_mapping.set_index("glofasStation", drop=False)
+
+        # Set up variables to fill
+        stations = []
+        trigger_per_day = {
+            '1-day': False,
+            '2-day': False,
+            '3-day': False,
+            '4-day': False,
+            '5-day': False,
+            '6-day': False,
+            '7-day': False,
+        }
+
+        for index, row in df_thresholds.iterrows():
+            station = {}
+            station['code'] = row['stationCode']
+
+            if station['code'] in df_district_mapping['glofasStation'] and station['code'] != 'no_station':
+                print(station['code'])
+                threshold = df_thresholds[df_thresholds['stationCode'] ==
+                                          station['code']][TRIGGER_LEVEL][0]
+
+                for step in range(1, 8):
+                    # Loop through 51 ensembles, get forecast and compare to threshold
+                    ensemble_options = 51
+                    count = 0
+                    dis_sum = 0
+
+                    for ensemble in range(1, ensemble_options):
+
                         # MOCK OVERWRITE DEPENDING ON COUNTRY SETTING
-                        if SETTINGS_SECRET[self.country_code]['mock'] == True:
-                            if SETTINGS_SECRET[self.country_code]['if_mock_trigger'] == True:
-                                if step < 5: # Only dummy trigger for 5-day and above
-                                    discharge = 0
-                                elif station['code'] == 'DWRM1':  # UGA dummy flood station 1
-                                    discharge = 1000
-                                elif station['code'] == 'G1067':  # ETH dummy flood station 1
-                                    discharge = 1000
-                                elif station['code'] == 'G1904':  # ETH dummy flood station 2
-                                    discharge = 2000
-                                elif station['code'] == 'G5194':  # KEN dummy flood station
-                                    discharge = 2000
-                                else:
-                                    discharge = 0
+                        if SETTINGS_SECRET[self.country_code]['if_mock_trigger'] == True:
+                            if step < 5: # Only dummy trigger for 5-day and above
+                                discharge = 0
+                            elif station['code'] == 'DWRM1':  # UGA dummy flood station 1
+                                discharge = 1000
+                            elif station['code'] == 'G1067':  # ETH dummy flood station 1
+                                discharge = 1000
+                            elif station['code'] == 'G1904':  # ETH dummy flood station 2
+                                discharge = 2000
+                            elif station['code'] == 'G5194':  # KEN dummy flood station
+                                discharge = 2000
+                            elif station['code'] == 'G1361':  # ZMB dummy flood station 1
+                                discharge = 8000
+                            elif station['code'] == 'G1328':  # ZMB dummy flood station 2
+                                discharge = 9000
                             else:
                                 discharge = 0
+                        else:
+                            discharge = 0
 
                         if discharge >= threshold:
                             count = count + 1
