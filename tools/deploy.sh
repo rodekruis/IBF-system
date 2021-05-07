ce72ed330b049b59c316bb11385a72d8f434d5d7
jannisvisser <jannisvisser@redcross.nl>
2021-05-07 15:20:21 +0200

    refactor: rm migrate_database step from deploy-script AB#7505

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -58,26 +58,6 @@ function deploy() {
         docker-compose --env-file /dev/null restart
     }
 
-    function migrate_database() {
-        if [ "$PRODUCTION_DATA_SERVER" = no ]; then
-            log "Migrate database..."
-
-            declare -a arr=("IBF-static-input")
-
-            for SCHEMA in "${arr[@]}"
-            do
-                echo "$SCHEMA"
-                rm -f tools/db-dumps/ibf_$SCHEMA.dump
-                PGPASSWORD=$DB_PASSWORD pg_dump -U $DB_USERNAME -Fc -f tools/db-dumps/ibf_$SCHEMA.dump -h $DB_HOST -n \"$SCHEMA\" geonode_datav3
-                PGPASSWORD=$DB_PASSWORD psql -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -c 'drop schema "'$SCHEMA'" cascade; create schema "'$SCHEMA'";'
-                PGPASSWORD=$DB_PASSWORD pg_restore -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST --schema=$SCHEMA --clean tools/db-dumps/ibf_$SCHEMA.dump
-            done
-
-            PGPASSWORD=$DB_PASSWORD psql -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -f $SQL_FILE_PATH
-        fi
-
-    }
-
     function restart_webhook_service() {
         log "Restart webhook service..."
 
@@ -118,8 +98,6 @@ function deploy() {
     load_environment_variables
 
     update_containers
-
-    migrate_database
 
     cleanup_docker
 
