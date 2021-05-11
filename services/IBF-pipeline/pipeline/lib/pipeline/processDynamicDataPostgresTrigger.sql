f0046066ecf40905e630e24de85cd5ab89212e69
jannisvisser <jannisvisser@redcross.nl>
2021-05-11 09:46:26 +0200

    temp commit (unfinished code)

--- services/IBF-pipeline/pipeline/lib/pipeline/processDynamicDataPostgresTrigger.sql
+++ services/IBF-pipeline/pipeline/lib/pipeline/processDynamicDataPostgresTrigger.sql
@@ -20,12 +20,13 @@ SELECT t0.id
 	, t1."forecastTrigger" as fc_trigger
 	, t1."forecastReturnPeriod" as fc_rp
 	, t1."date" 
-	, t1."countryCode" as country_code
+	, t0."countryCode" as country_code
 	, t1."leadTime" as lead_time
 into "IBF-pipeline-output".triggers_rp_per_station
 FROM "IBF-app"."glofasStation" t0
 LEFT JOIN "IBF-app"."glofasStationTrigger" t1 ON t0."stationCode" = t1."stationCode"
 ;
+--select * from "IBF-pipeline-output".triggers_rp_per_station;
 
 CREATE TABLE if not exists "IBF-pipeline-output".dashboard_forecast_per_station (
     country_code varchar NULL,
@@ -55,7 +56,8 @@ SELECT t0."countryCode" as country_code,
     fc_prob as fc_prob,
     st_astext(t0.geom)
 FROM "IBF-app"."glofasStation" t0
-    LEFT JOIN "IBF-pipeline-output".triggers_rp_per_station t1 ON t0."stationCode" = t1."stationCode"
+LEFT JOIN "IBF-pipeline-output".triggers_rp_per_station t1 
+	ON t0."stationCode" = t1."stationCode"
     AND t0."countryCode" = t1.country_code
 where date >= current_date;
 --select * from "IBF-pipeline-output".dashboard_forecast_per_station order by 1
@@ -71,7 +73,8 @@ select t0."countryCode" as country_code,
     fc_rp,
     fc_prob INTO "IBF-pipeline-output".dashboard_forecast_per_district
 FROM "IBF-app"."adminArea" t0
-    LEFT JOIN "IBF-pipeline-output".dashboard_forecast_per_station t1 ON t0."glofasStation" = t1.station_code
+LEFT JOIN "IBF-pipeline-output".dashboard_forecast_per_station t1 
+	ON t0."glofasStation" = t1.station_code
     and t0."countryCode" = t1.country_code
 where t1.lead_time is not null;
 --select * from "IBF-pipeline-output".dashboard_forecast_per_district;
