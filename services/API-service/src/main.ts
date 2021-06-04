ba794bf3160cd2aaea7b917064d98d9ad73b4b7d
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-06-04 16:57:00 +0200

    docs: update api documentation description AB#8027

--- services/API-service/src/main.ts
+++ services/API-service/src/main.ts
@@ -1,22 +1,30 @@
 import { PORT, SCHEME } from './config';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const appOptions = { cors: true };
   const app = await NestFactory.create(ApplicationModule, appOptions);
+  const apiVersion = '1.0';
   app.setGlobalPrefix('api');
 
-  const options = new DocumentBuilder()
-    .setTitle('IBF - API-service')
-    .setDescription('API description')
-    .setVersion('1.0')
+  const config = new DocumentBuilder()
+    .setTitle('IBF API')
+    .setDescription(
+      'This page serves as the documentation of IBF API endpoints. Use the `/api/user/login` endpoint for the `Authorization Token` to access protected data.',
+    )
+    .setVersion(apiVersion)
     .addServer(`${SCHEME}://`)
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, options);
+  const options: SwaggerDocumentOptions = {};
+  const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('/docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || PORT);
