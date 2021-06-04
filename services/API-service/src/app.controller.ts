614545945d9bfd1e5656c43a688b8b00ef9ecf00
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-06-04 16:59:23 +0200

    docs: remove unused imports AB#8028

--- services/API-service/src/app.controller.ts
+++ services/API-service/src/app.controller.ts
@@ -1,11 +1,5 @@
 import { Get, Controller } from '@nestjs/common';
-import {
-  ApiHeader,
-  ApiHeaderOptions,
-  ApiOperation,
-  ApiResponse,
-  ApiResponseOptions,
-} from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
