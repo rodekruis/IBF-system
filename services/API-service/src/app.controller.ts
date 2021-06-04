7ea72d57f1c32ce00f3fe63a80d67850de1cedf3
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-06-04 16:58:15 +0200

    docs: clean root api AB#8028

--- services/API-service/src/app.controller.ts
+++ services/API-service/src/app.controller.ts
@@ -1,8 +1,20 @@
 import { Get, Controller } from '@nestjs/common';
+import {
+  ApiHeader,
+  ApiHeaderOptions,
+  ApiOperation,
+  ApiResponse,
+  ApiResponseOptions,
+} from '@nestjs/swagger';
 
-@Controller('api')
+@Controller()
 export class AppController {
+  @ApiOperation({ summary: 'Check API access' })
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Response body is the text "Hello World!"',
+  })
   public root(): string {
     return 'Hello World!';
   }
