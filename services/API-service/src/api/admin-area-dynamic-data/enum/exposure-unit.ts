3698126c49e9fdbbe4917d04745928a4210a9819
RubenGeo <rubenavalk@gmail.com>
2021-06-16 11:16:12 +0200

    fix: Bug in IBF floods with population affected selector

--- services/API-service/src/api/admin-area-dynamic-data/enum/exposure-unit.ts
+++ services/API-service/src/api/admin-area-dynamic-data/enum/exposure-unit.ts
@@ -1,5 +1,5 @@
 export enum ExposureUnit {
-  population = 'population',
+  populationAffected = 'population_affected',
   potentialCasesU9 = 'potential_cases_U9',
   potentialCases65 = 'potential_cases_65',
   alertThreshold = 'alert_threshold',
