a245cabd7031b373e16760161f71f3474d8d0fd0
RubenGeo <rubenavalk@gmail.com>
2021-06-29 16:33:13 +0200

    fix: tests

--- services/API-service/src/api/notification/notification.controller.spec.ts
+++ services/API-service/src/api/notification/notification.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from '../../mock/repositoryMock.factory';
+import { UserEntity } from '../user/user.entity';
+import { UserService } from '../user/user.service';
 import { NotificationController } from './notification.controller';
 
 describe('NotificationController', () => {
@@ -7,6 +11,13 @@ describe('NotificationController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationController],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     controller = module.get<NotificationController>(NotificationController);
--- services/API-service/src/api/notification/notification.service.spec.ts
+++ services/API-service/src/api/notification/notification.service.spec.ts
@@ -1,4 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from '../../mock/repositoryMock.factory';
+import { AdminAreaDynamicDataEntity } from '../admin-area-dynamic-data/admin-area-dynamic-data.entity';
+import { AdminAreaDynamicDataService } from '../admin-area-dynamic-data/admin-area-dynamic-data.service';
+import { CountryEntity } from '../country/country.entity';
+import { CountryService } from '../country/country.service';
+import { AreaOfFocusEntity } from '../eap-actions/area-of-focus.entity';
+import { EapActionStatusEntity } from '../eap-actions/eap-action-status.entity';
+import { EapActionEntity } from '../eap-actions/eap-action.entity';
+import { EapActionsService } from '../eap-actions/eap-actions.service';
+import { EventPlaceCodeEntity } from '../event/event-place-code.entity';
+import { EventService } from '../event/event.service';
+import { TriggerPerLeadTime } from '../event/trigger-per-lead-time.entity';
+import { IndicatorMetadataEntity } from '../metadata/indicator-metadata.entity';
+import { UserEntity } from '../user/user.entity';
 import { NotificationService } from './notification.service';
 
 describe('NotificationService', () => {
@@ -6,7 +21,49 @@ describe('NotificationService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationService],
+      providers: [
+        {
+          provide: getRepositoryToken(EventPlaceCodeEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(AdminAreaDynamicDataEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(TriggerPerLeadTime),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(CountryEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(EapActionStatusEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(EapActionEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(AreaOfFocusEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(IndicatorMetadataEntity),
+          useFactory: repositoryMockFactory,
+        },
+        EventService,
+        CountryService,
+        EapActionsService,
+        NotificationService,
+        AdminAreaDynamicDataService,
+      ],
     }).compile();
 
     service = module.get<NotificationService>(NotificationService);
