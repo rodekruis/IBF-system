796022e2b7b09c1b279709ec2f772a4feb68cda2
jannisvisser <jannisvisser@redcross.nl>
2021-07-06 17:06:31 +0200

    fix: add missing migration-script AB#8471

---
+++ services/API-service/migration/1624855398157-DisasterTypeLayers.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class DisasterTypeLayers1624855398157 implements MigrationInterface {
+    name = 'DisasterTypeLayers1624855398157'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "IBF-app"."indicator-metadata" ADD "disasterType" character varying`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."layer-metadata" ADD "disasterType" character varying`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."lead-time"."created" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."admin-area"."geom" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."admin-area" ALTER COLUMN "geom" TYPE geometry`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."eap-action-status"."timestamp" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."user"."created" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."adminLevels" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."country" ALTER COLUMN "adminLevels" SET DEFAULT array[]::int[]`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."countryLogos" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."country" ALTER COLUMN "countryLogos" SET DEFAULT array[]::text[]`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."countryBoundingBox" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."country" ALTER COLUMN "countryBoundingBox" TYPE geometry`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."created" IS NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."created" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."country" ALTER COLUMN "countryBoundingBox" TYPE geometry(GEOMETRY,0)`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."countryBoundingBox" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."country" ALTER COLUMN "countryLogos" SET DEFAULT ARRAY[]`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."countryLogos" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."country" ALTER COLUMN "adminLevels" SET DEFAULT ARRAY[]`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."country"."adminLevels" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."user"."created" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."eap-action-status"."timestamp" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."admin-area" ALTER COLUMN "geom" TYPE geometry(GEOMETRY,0)`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."admin-area"."geom" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "IBF-app"."lead-time"."created" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."layer-metadata" DROP COLUMN "disasterType"`);
+        await queryRunner.query(`ALTER TABLE "IBF-app"."indicator-metadata" DROP COLUMN "disasterType"`);
+    }
+
+}
