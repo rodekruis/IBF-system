af08d12968de010330dbb0d583f12a4a2298fcf9
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-07-15 14:35:23 +0200

    feat: add potential cases under 5 layer for malaria

--- services/API-service/src/api/admin-area-dynamic-data/enum/dynamic-data-unit.ts
+++ services/API-service/src/api/admin-area-dynamic-data/enum/dynamic-data-unit.ts
@@ -1,6 +1,7 @@
 export enum DynamicIndicator {
   populationAffected = 'population_affected',
   potentialCasesU9 = 'potential_cases_U9',
+  potentialCasesU5 = 'potential_cases_U5',
   potentialCases65 = 'potential_cases_65',
   alertThreshold = 'alert_threshold',
   potentialCases = 'potential_cases',
