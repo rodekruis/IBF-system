12bb4657a86057ffcb986b6740c5a154989a692d
jannisvisser <jannisvisser@redcross.nl>
2021-08-06 16:33:34 +0200

    refactor: add better types to update dto AB#8842

--- services/API-service/src/api/admin-area-data/dto/upload-admin-area-data.dto.ts
+++ services/API-service/src/api/admin-area-data/dto/upload-admin-area-data.dto.ts
@@ -11,6 +11,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { DynamicDataPlaceCodeDto } from '../../admin-area-dynamic-data/dto/dynamic-data-place-code.dto';
 import indicatorData from '../../admin-area-dynamic-data/dto/example/upload-exposure-ETH-triggered.json';
 import { DynamicIndicator } from '../../admin-area-dynamic-data/enum/dynamic-data-unit';
+import { AdminLevel } from '../../country/admin-level.enum';
+import { JoinColumn, ManyToOne } from 'typeorm';
+import { CountryEntity } from '../../country/country.entity';
 
 export class UploadAdminAreaDataCsvDto {
   @ApiProperty()
@@ -42,12 +45,17 @@ export class UploadAdminAreaDataJsonDto {
   @ApiProperty({ example: 'ETH' })
   @IsNotEmpty()
   @IsString()
+  @ManyToOne((): typeof CountryEntity => CountryEntity)
+  @JoinColumn({
+    name: 'countryCodeISO3',
+    referencedColumnName: 'countryCodeISO3',
+  })
   public countryCodeISO3: string;
 
   @ApiProperty({ example: 3 })
   @IsNotEmpty()
   @IsNumber()
-  public adminLevel: number;
+  public adminLevel: AdminLevel;
 
   @ApiProperty({ example: 'Hotspot_General' })
   @IsNotEmpty()
