fbaba2af8ac549c44560eca336196797d2812952
SUSAGRAW <87130992+SUSAGRAW@users.noreply.github.com>
2021-08-27 16:05:49 +0200

    Update Chat Component

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -24,7 +24,28 @@ Cypress.Commands.add("login", () => {
   cy.waitForRequests();
   cy.waitUntil(() => Cypress.$(selectors.loader).length === 0); // https://github.com/NoriSte/cypress-wait-until/issues/75#issuecomment-572685623
 });
-
+/* close all popup windows */
+Cypress.Commands.add('closeAllTabs', () => {
+  if (!myTabs.length) {
+      return;
+  }
+  myTabs.forEach((v, k) => {
+      if (k > 0) {
+          try {
+              myTabs[k].close()
+          } catch (e) {
+              console.error(e)
+          }
+          myTabs[k] = null;
+      }
+  })
+  myTabNames.splice(1)
+  myTabs.splice(1) // keep first one only
+  // return to state 0 (main / root / original window)
+  active_tab_index = 0;
+  cy.state('document', myTabs[0].document)
+  cy.state('window', myTabs[0])
+})
 // Function that waits fo all promises to resolve
 Cypress.Commands.add("waitForAngular", () => {
   return cy
