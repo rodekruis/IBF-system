899347adc3b3c175d8c96e183ecbaa431f195aff
jannisvisser <jannisvisser@redcross.nl>
2021-09-24 15:31:38 +0200

    docs: improve login explanation swagger AB#9844

--- services/API-service/src/main.ts
+++ services/API-service/src/main.ts
@@ -17,7 +17,8 @@ async function bootstrap(): Promise<void> {
   const apiDocumentationFavicon =
     'https://www.510.global/wp-content/uploads/2017/09/cropped-510-FLAVICON-01-32x32.png';
   const apiDocumentationDescription =
-    'This page serves as the documentation of IBF API endpoints. Use the `/api/user/login` endpoint for the `Authorization Token` to access protected data.';
+    'This page serves as the documentation of IBF API endpoints, and can also be used for executing endpoints.<br>To get access:<ul><li>If you have an account, use the `/api/user/login` endpoint below to get a token and copy-paste it in the `Authorize` button on the top right of this page.</li><li>If you do not have an account, contact the IBF Development Team.</li><li>Verify your access by using the `check API` endpoints below</li></ul>';
+
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
