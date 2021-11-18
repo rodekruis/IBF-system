2c6d609bf3de021c7dce7cbdbebba492d455de88
swati.thakre <swati.thakre@amac02d24cgml7l.home>
2021-11-18 15:11:37 +0100

    feat: added new title segment 9121-sthakre

--- interfaces/IBF-dashboard/src/app/components/user-state/user-state.component.ts
+++ interfaces/IBF-dashboard/src/app/components/user-state/user-state.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   AnalyticsEvent,
   AnalyticsPage,
@@ -7,21 +7,51 @@ import { AnalyticsService } from 'src/app/analytics/analytics.service';
 import { AuthService } from 'src/app/auth/auth.service';
 import { EventService } from 'src/app/services/event.service';
 import { LoaderService } from 'src/app/services/loader.service';
+import { DisasterTypeService } from 'src/app/services/disaster-type.service';
+import { CountryService } from 'src/app/services/country.service';
+import { Country,DisasterType } from 'src/app/models/country.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-state',
   templateUrl: './user-state.component.html',
   styleUrls: ['./user-state.component.scss'],
 })
-export class UserStateComponent {
+export class UserStateComponent implements OnInit {
   public displayName: string;
+  public country: Country;
+  public countrySubscription: Subscription;
+  public disasterTypeSubscription: Subscription;
+  public disasterType: string;
 
   constructor(
     public authService: AuthService,
     private loaderService: LoaderService,
     private analyticsService: AnalyticsService,
     private eventService: EventService,
-  ) {}
+    public disasterTypeService: DisasterTypeService,
+    public countryService: CountryService,
+    
+  ) {
+    
+  }
+
+  ngOnInit() {
+    this.countrySubscription = this.countryService
+      .getCountrySubscription()
+      .subscribe(this.onCountryChange);
+      this.disasterTypeSubscription = this.disasterTypeService
+      .getDisasterTypeSubscription()
+      .subscribe(this.onDisasterTypeChange);
+  }
+
+  private onCountryChange = (country: Country) => {
+    this.country = country;
+  };
+
+  private onDisasterTypeChange = (disasterType: DisasterType) => {
+    this.disasterType = disasterType?.disasterType?.toUpperCase();
+  };
 
   public doLogout() {
     this.analyticsService.logEvent(AnalyticsEvent.logOut, {
