5c4f1d6087b41bf4e97c33577b0a8e0331df25d5
jannisvisser <jannisvisser@redcross.nl>
2021-12-13 09:32:10 +0100

    test

--- services/API-service/src/api/user/user.controller.spec.ts
+++ services/API-service/src/api/user/user.controller.spec.ts
@@ -38,6 +38,7 @@ class UserServiceMock {
     user.email = 'test@ibf.nl';
     user.password =
       'c90f86e09c3461da52b3d8bc80ccd6a0d0cb893b1a41bd461e8ed31fa21c9b6e';
+    user.userRole = UserRole.DisasterManager;
     return user;
   }
   public generateJWT(): string {
