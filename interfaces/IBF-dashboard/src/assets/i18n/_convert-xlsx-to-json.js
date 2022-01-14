12f6060b7bbb4e069e0131b8c7035666e8cbc75b
jannisvisser <jannisvisser@redcross.nl>
2022-01-14 17:17:01 +0100

    lint

--- interfaces/IBF-dashboard/src/assets/i18n/_convert-xlsx-to-json.js
+++ interfaces/IBF-dashboard/src/assets/i18n/_convert-xlsx-to-json.js
@@ -6,27 +6,26 @@ var workbook = XLSX.readFile('layer-popup-info.xlsx');
 
 var worksheet = workbook.Sheets['Sheet1'];
 var data = '';
-for(z in worksheet) {
-    if(z[0] === '!') continue;
-    //parse out the column, row, and value
-    var tt = 0;
-    for (var i = 0; i < z.length; i++) {
-        if (!isNaN(z[i])) {
-            tt = i;
-            break;
-        }
-    };
-    var col = z.substring(0,tt);
-    if (col === 'P') {
-      var value = worksheet[z].v;
-      data += value;
+for (z in worksheet) {
+  if (z[0] === '!') continue;
+  //parse out the column, row, and value
+  var tt = 0;
+  for (var i = 0; i < z.length; i++) {
+    if (!isNaN(z[i])) {
+      tt = i;
+      break;
     }
+  }
+  var col = z.substring(0, tt);
+  if (col === 'P') {
+    var value = worksheet[z].v;
+    data += value;
+  }
 }
 
-fs.writeFile("layer-popup-info.json", data, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-}); 
-
+fs.writeFile('layer-popup-info.json', data, function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('The file was saved!');
+});
