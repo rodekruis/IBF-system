2e14ec2a40e94c097bb3b23ef996863702a71968
arsforza <asforza@redcross.nl>
2022-02-18 17:11:54 +0100

    fix: lowercase email on create user AB#12447

--- services/API-service/src/api/user/user.service.ts
+++ services/API-service/src/api/user/user.service.ts
@@ -41,7 +41,8 @@ export class UserService {
 
   public async create(dto: CreateUserDto): Promise<UserResponseObject> {
     // check uniqueness of username
-    const { email, password } = dto;
+    let { email, password } = dto;
+    email = email.toLowerCase();
     const qb = await getRepository(UserEntity)
       .createQueryBuilder('user')
       .where('user.email = :email', { email });
