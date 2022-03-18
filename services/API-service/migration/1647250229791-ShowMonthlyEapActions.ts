1763e4ff49cab764af6d8f6e9ce13ecced84408c
arsforza <asforza@redcross.nl>
2022-03-18 12:40:40 +0100

    fix: combine migration scripts AB#12974

--- services/API-service/migration/1647250229791-ShowMonthlyEapActions.ts
+++ services/API-service/migration/1647250229791-ShowMonthlyEapActions.ts
@@ -7,9 +7,17 @@ export class ShowMonthlyEapActions1647250229791 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "IBF-app"."country-disaster-settings" ADD "showMonthlyEapActions" boolean NOT NULL DEFAULT false`,
     );
+
+    await queryRunner.query(
+      `ALTER TABLE "IBF-app"."country-disaster-settings" ADD "monthlyForecastInfo" json DEFAULT null`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "IBF-app"."country-disaster-settings" DROP COLUMN "monthlyForecastInfo"`,
+    );
+
     await queryRunner.query(
       `ALTER TABLE "IBF-app"."country-disaster-settings" DROP COLUMN "showMonthlyEapActions"`,
     );
