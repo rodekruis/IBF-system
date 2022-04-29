9b6f9482456f454cc470262dd749633d3445f2a7
jannisvisser <jannisvisser@redcross.nl>
2022-04-29 10:07:13 +0200

    fix: test across multiple disaster-types AB#13500

--- tests/cypress/integration/IBF-dashboard/timeline-component.spec.js
+++ tests/cypress/integration/IBF-dashboard/timeline-component.spec.js
@@ -1,56 +1,46 @@
 import selectors from '../../support/selectors';
 
+Cypress.Commands.add('executeTimelineTests', () => {
+  // should load at least 1 active (enabled) timeline button
+  cy.get(selectors.timeline.timelineButton).should('not.be.disabled');
+
+  // should load a triggered button if and only if the dashboard is in triggered state
+  cy.isStatusTriggered().then((triggered) => {
+    triggered
+      ? cy.get(`${selectors.timeline.timelineButton}.alert`).should('exist')
+      : cy
+          .get(`${selectors.timeline.timelineButton}.alert`)
+          .should('not.exist');
+  });
+
+  // should have only 1 selected button
+  cy.get(`${selectors.timeline.timelineButton}.active`).should(
+    'have.length',
+    1,
+  );
+});
+
 describe('timeline component', () => {
-  it('should login', () => {
+  beforeEach(() => {
     cy.login();
   });
 
   // Tests for 1st disaster-type
-  it('DISASTER TYPE 1: should load at least 1 active (enabled) timeline button', () => {
-    cy.get(selectors.timeline.timelineButton).should('not.be.disabled');
-  });
+  it('Disaster type 1: validate timeline buttons', () => {
+    // click 1st disaster-type not necessary as it is loaded by default
+    // wait for all requests to finish
+    cy.wait(5000);
 
-  it('DISASTER TYPE 1: should load a triggered button if and only if the dashboard is in triggered state', () => {
-    cy.isStatusTriggered().then((triggered) => {
-      triggered
-        ? cy.get(`${selectors.timeline.timelineButton}.alert`)
-        : cy
-            .get(`${selectors.timeline.timelineButton}.alert`)
-            .should('not.exist');
-    });
-  });
-
-  it('DISASTER TYPE 1: should have only 1 selected button', () => {
-    cy.get(`${selectors.timeline.timelineButton}.active`).should(
-      'have.length',
-      1,
-    );
-  });
-
-  // click 2nd disaster-type
-  it('click on 2nd disaster-type', () => {
-    cy.get(selectors.disasterType.disasterTypeButtons).eq(1).click();
+    cy.executeTimelineTests();
   });
 
   // Tests for 2nd disaster-type
-  it('DISASTER TYPE 2: should load at least 1 active (enabled) timeline button', () => {
-    cy.get(selectors.timeline.timelineButton).should('not.be.disabled');
-  });
+  it('Disaster type 2: validate timeline buttons', () => {
+    // click 2nd disaster-type
+    cy.get(selectors.disasterType.disasterTypeButtons).eq(1).click();
+    // wait for all requests to finish
+    cy.wait(15000);
 
-  it('DISASTER TYPE 2: should load a triggered button if and only if the dashboard is in triggered state', () => {
-    cy.isStatusTriggered().then((triggered) => {
-      triggered
-        ? cy.get(`${selectors.timeline.timelineButton}.alert`).should('exist')
-        : cy
-            .get(`${selectors.timeline.timelineButton}.alert`)
-            .should('not.exist');
-    });
-  });
-
-  it('DISASTER TYPE 2: should have only 1 selected button', () => {
-    cy.get(`${selectors.timeline.timelineButton}.active`).should(
-      'have.length',
-      1,
-    );
+    cy.executeTimelineTests();
   });
 });
