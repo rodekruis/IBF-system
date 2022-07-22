bb7fe7a86e4e826b3c5dfad187620a594942d710
arsforza <asforza@redcross.nl>
2022-07-22 09:54:15 +0200

    feat: add action-result popover AB#14484

---
+++ interfaces/IBF-dashboard/src/app/components/action-result-popover/action-result-popover.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ActionResultPopoverComponent } from './action-result-popover.component';
+
+describe('ActionResultPopoverComponent', () => {
+  let component: ActionResultPopoverComponent;
+  let fixture: ComponentFixture<ActionResultPopoverComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActionResultPopoverComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ActionResultPopoverComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
