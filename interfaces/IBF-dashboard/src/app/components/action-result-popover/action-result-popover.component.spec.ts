86574263b51edaa96447db53d05e373678d7d1c1
arsforza <asforza@redcross.nl>
2022-07-22 12:54:52 +0200

    fix: test + prettier AB#14484

--- interfaces/IBF-dashboard/src/app/components/action-result-popover/action-result-popover.component.spec.ts
+++ interfaces/IBF-dashboard/src/app/components/action-result-popover/action-result-popover.component.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ActionResultPopoverComponent } from './action-result-popover.component';
 
 describe('ActionResultPopoverComponent', () => {
   let component: ActionResultPopoverComponent;
   let fixture: ComponentFixture<ActionResultPopoverComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ActionResultPopoverComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ActionResultPopoverComponent],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ActionResultPopoverComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(ActionResultPopoverComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }),
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
