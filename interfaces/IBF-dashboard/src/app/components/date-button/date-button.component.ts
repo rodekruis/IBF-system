0470712cb93bd62fecd7e58b7ec7836f09d0bf51
arsforza <asforza@redcross.nl>
2023-06-02 16:39:55 +0200

    fix: refactor date bt AB#19465

--- interfaces/IBF-dashboard/src/app/components/date-button/date-button.component.ts
+++ interfaces/IBF-dashboard/src/app/components/date-button/date-button.component.ts
@@ -2,15 +2,9 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { DateTime } from 'luxon';
 import { Subscription } from 'rxjs';
 import { DisasterTypeService } from 'src/app/services/disaster-type.service';
-import {
-  DateFormats,
-  LeadTimeUnit,
-  MonthFormats,
-} from 'src/app/types/lead-time';
+import { DateFormats, MonthFormats } from 'src/app/types/lead-time';
 import { TimelineService } from '../../services/timeline.service';
 import { DisasterTypeKey } from '../../types/disaster-type-key';
-import { TimelineState } from '../../types/timeline-state';
-
 @Component({
   selector: 'app-date-button',
   templateUrl: './date-button.component.html',
@@ -18,20 +12,18 @@ import { TimelineState } from '../../types/timeline-state';
 })
 export class DateButtonComponent implements OnInit, OnDestroy {
   @Input() date = DateTime.now();
-  @Input() unit = LeadTimeUnit.day;
   @Input() active: boolean;
   @Input() alert: boolean;
   @Input() thresholdReached: boolean;
-  @Input() todayButton: boolean;
   @Input() eventName: string | null;
   @Input() duration: number | null;
 
   private dateFormat = '';
   private monthFormat = '';
   private hourFormat = 'HH:00 a';
-  public displayDate: string;
-  public displayMonth: string;
-  public displayHour: string;
+  public firstLine: string;
+  public secondLine: string;
+  public thirdLine: string;
 
   private timelineStateSubscription: Subscription;
 
@@ -50,45 +42,48 @@ export class DateButtonComponent implements OnInit, OnDestroy {
     this.timelineStateSubscription.unsubscribe();
   }
 
-  private onTimelineStateChange = (timelineState: TimelineState) => {
-    if (this.todayButton) {
-      this.date = timelineState.today;
-    }
+  private onTimelineStateChange = () => {
     const disasterType = this.disasterTypeService?.disasterType?.disasterType;
 
     this.dateFormat = DateFormats[disasterType] || DateFormats.default;
     this.monthFormat = MonthFormats[disasterType] || MonthFormats.default;
     if (
-      this.unit === LeadTimeUnit.day ||
-      disasterType === DisasterTypeKey.flashFloods
+      [
+        DisasterTypeKey.flashFloods,
+        DisasterTypeKey.floods,
+        DisasterTypeKey.heavyRain,
+      ].includes(disasterType)
     ) {
-      this.displayDate = this.date.toFormat(this.dateFormat);
+      this.firstLine = this.date.toFormat(this.dateFormat);
     }
-    if (
-      this.unit === LeadTimeUnit.hour &&
-      disasterType !== DisasterTypeKey.flashFloods
-    ) {
+
+    if (disasterType === DisasterTypeKey.typhoon) {
       if (this.active) {
-        this.displayHour = this.date
+        this.thirdLine = this.date
           ? this.date.toFormat(this.hourFormat)
           : 'Landfall';
       } else {
-        this.displayHour = '';
+        this.thirdLine = '';
       }
     }
-    this.displayMonth = this.date
+
+    this.secondLine = this.date
       ? this.date.toFormat(this.monthFormat)
       : 'Undetermined';
 
-    if (this.eventName && this.duration && this.unit === LeadTimeUnit.month) {
+    if (
+      this.eventName &&
+      this.duration &&
+      disasterType === DisasterTypeKey.drought
+    ) {
       const endMonthDate = this.date.plus({ months: this.duration - 1 });
       let displayMonth = this.date.monthShort;
       if (this.duration > 1) {
         displayMonth = `${displayMonth}-${endMonthDate.monthShort}`;
       }
-      this.displayMonth = `${displayMonth} ${endMonthDate.year}`;
+      this.secondLine = `${displayMonth} ${endMonthDate.year}`;
 
-      this.displayHour = `Duration ${this.duration} months`;
+      this.thirdLine = `Duration ${this.duration} months`;
     }
   };
 }
