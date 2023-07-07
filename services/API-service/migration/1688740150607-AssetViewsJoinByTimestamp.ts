9d69beea2593e74476f7871cbe6dd749226ea78c
jannisvisser <jannisvisser@redcross.nl>
2023-07-07 16:37:50 +0200

    fix: join by timestamp AB#20149

---
+++ services/API-service/migration/1688740150607-AssetViewsJoinByTimestamp.ts
@@ -0,0 +1,93 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AssetViewsJoinByTimestamp1688740150607
+  implements MigrationInterface {
+  name = 'AssetViewsJoinByTimestamp1688740150607';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // delete views first
+    await queryRunner.query(
+      `DELETE FROM "IBF-app"."typeorm_metadata" WHERE "type" = 'VIEW' AND "schema" = $1 AND "name" = $2`,
+      ['IBF-app', 'roads_exposure_per_lead_time'],
+    );
+    await queryRunner.query(
+      `DROP VIEW "IBF-app"."roads_exposure_per_lead_time"`,
+    );
+    await queryRunner.query(
+      `DELETE FROM "IBF-app"."typeorm_metadata" WHERE "type" = 'VIEW' AND "schema" = $1 AND "name" = $2`,
+      ['IBF-app', 'buildings_exposure_per_lead_time'],
+    );
+    await queryRunner.query(
+      `DROP VIEW "IBF-app"."buildings_exposure_per_lead_time"`,
+    );
+
+    // then recreate with new definition
+    await queryRunner.query(
+      `CREATE VIEW "IBF-app"."buildings_exposure_per_lead_time" AS SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId"  LEFT JOIN (SELECT MAX(timestamp) as max_timestamp FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = 'buildings') "max_timestamp" ON 1=1 WHERE line."linesDataCategory" = 'buildings' AND ("status"."timestamp" = max_timestamp.max_timestamp OR "status"."timestamp" IS NULL)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "IBF-app"."typeorm_metadata"("type", "schema", "name", "value") VALUES ($1, $2, $3, $4)`,
+      [
+        'VIEW',
+        'IBF-app',
+        'buildings_exposure_per_lead_time',
+        'SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId"  LEFT JOIN (SELECT MAX(timestamp) as max_timestamp FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = \'buildings\') "max_timestamp" ON 1=1 WHERE line."linesDataCategory" = \'buildings\' AND ("status"."timestamp" = max_timestamp.max_timestamp OR "status"."timestamp" IS NULL)',
+      ],
+    );
+    await queryRunner.query(
+      `CREATE VIEW "IBF-app"."roads_exposure_per_lead_time" AS SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId"  LEFT JOIN (SELECT MAX(timestamp) as max_timestamp FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = 'roads') "max_timestamp" ON 1=1 WHERE line."linesDataCategory" = 'roads' AND ("status"."timestamp" = max_timestamp.max_timestamp OR "status"."timestamp" IS NULL)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "IBF-app"."typeorm_metadata"("type", "schema", "name", "value") VALUES ($1, $2, $3, $4)`,
+      [
+        'VIEW',
+        'IBF-app',
+        'roads_exposure_per_lead_time',
+        'SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId"  LEFT JOIN (SELECT MAX(timestamp) as max_timestamp FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = \'roads\') "max_timestamp" ON 1=1 WHERE line."linesDataCategory" = \'roads\' AND ("status"."timestamp" = max_timestamp.max_timestamp OR "status"."timestamp" IS NULL)',
+      ],
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DELETE FROM "IBF-app"."typeorm_metadata" WHERE "type" = 'VIEW' AND "schema" = $1 AND "name" = $2`,
+      ['IBF-app', 'roads_exposure_per_lead_time'],
+    );
+    await queryRunner.query(
+      `DROP VIEW "IBF-app"."roads_exposure_per_lead_time"`,
+    );
+    await queryRunner.query(
+      `DELETE FROM "IBF-app"."typeorm_metadata" WHERE "type" = 'VIEW' AND "schema" = $1 AND "name" = $2`,
+      ['IBF-app', 'buildings_exposure_per_lead_time'],
+    );
+    await queryRunner.query(
+      `DROP VIEW "IBF-app"."buildings_exposure_per_lead_time"`,
+    );
+
+    // recreate with old definition (copied from previous migration file)
+    await queryRunner.query(
+      `CREATE VIEW "IBF-app"."buildings_exposure_per_lead_time" AS SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = 'buildings' ORDER BY "status"."timestamp" DESC`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "IBF-app"."typeorm_metadata"("type", "schema", "name", "value") VALUES ($1, $2, $3, $4)`,
+      [
+        'VIEW',
+        'IBF-app',
+        'buildings_exposure_per_lead_time',
+        'SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = \'buildings\' ORDER BY "status"."timestamp" DESC',
+      ],
+    );
+    await queryRunner.query(
+      `CREATE VIEW "IBF-app"."roads_exposure_per_lead_time" AS SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = 'roads' ORDER BY "status"."timestamp" DESC`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "IBF-app"."typeorm_metadata"("type", "schema", "name", "value") VALUES ($1, $2, $3, $4)`,
+      [
+        'VIEW',
+        'IBF-app',
+        'roads_exposure_per_lead_time',
+        'SELECT line."referenceId",line.geom, status."leadTime", COALESCE("status"."exposed",FALSE) as "exposed" FROM "IBF-app"."lines-data" "line" LEFT JOIN "IBF-app"."lines-data-dynamic-status" "status" ON line."linesDataId" = status."referenceId" WHERE line."linesDataCategory" = \'roads\' ORDER BY "status"."timestamp" DESC',
+      ],
+    );
+  }
+}
--- services/API-service/src/api/lines-data/lines-data-views.entity.ts
+++ services/API-service/src/api/lines-data/lines-data-views.entity.ts
@@ -13,12 +13,29 @@ const getViewQuery = (type: LinesDataEnum) => {
         'status',
         'line."linesDataId" = status."referenceId"',
       )
+      .leftJoin(
+        subquery => {
+          return subquery
+            .select('MAX(timestamp) as max_timestamp')
+            .from(LinesDataEntity, 'line')
+            .leftJoin(
+              LinesDataDynamicStatusEntity,
+              'status',
+              'line."linesDataId" = status."referenceId"',
+            )
+            .where(`line."linesDataCategory" = '${type}'`);
+        },
+        'max_timestamp',
+        '1=1',
+      )
       .where(`line."linesDataCategory" = '${type}'`)
+      .andWhere(
+        '(status.timestamp = max_timestamp.max_timestamp OR status.timestamp IS NULL)',
+      )
       .addSelect([
         'status."leadTime"',
         'COALESCE(status.exposed,FALSE) as "exposed"',
-      ])
-      .orderBy('status.timestamp', 'DESC');
+      ]);
 };
 @ViewEntity({
   expression: getViewQuery(LinesDataEnum.buildings),
