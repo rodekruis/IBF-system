414abcfb161653c20fec497199ca89c2e8b5d968
jannisvisser <jannisvisser@redcross.nl>
2023-07-31 11:01:47 +0200

    unit test

--- interfaces/IBF-dashboard/src/app/services/map-legend.service.spec.ts
+++ interfaces/IBF-dashboard/src/app/services/map-legend.service.spec.ts
@@ -1,11 +1,15 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MapLegendService } from './map-legend.service';
 
 describe('MapLegendService', () => {
   let service: MapLegendService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    });
     service = TestBed.inject(MapLegendService);
   });
 
