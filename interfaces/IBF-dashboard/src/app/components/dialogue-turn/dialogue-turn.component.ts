e9b567a8c62cd20a617f99d6117e904c0d2ed25b
jannisvisser <jannisvisser@redcross.nl>
2023-08-07 15:36:15 +0200

    fix: no timestamp in speech bubble AB#22947

--- interfaces/IBF-dashboard/src/app/components/dialogue-turn/dialogue-turn.component.ts
+++ interfaces/IBF-dashboard/src/app/components/dialogue-turn/dialogue-turn.component.ts
@@ -19,7 +19,7 @@ export class DialogueTurnComponent implements OnInit {
   actor = Actor.system;
 
   @Input()
-  timestamp: DateTime = DateTime.now();
+  timestamp: DateTime;
 
   @Input()
   isConnected = false;
@@ -51,7 +51,6 @@ export class DialogueTurnComponent implements OnInit {
   ngOnInit() {
     this.isSelf = this.actor === Actor.self;
     this.isSystem = this.actor === Actor.system;
-    this.timestamp = DateTime.now();
   }
 
   show() {
