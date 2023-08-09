4b3278b6f9bcc0e4526a204a0847b6c316347480
jannisvisser <jannisvisser@redcross.nl>
2023-08-09 09:40:06 +0200

    unit test

--- interfaces/IBF-dashboard/src/app/components/tooltip/tooltip.component.spec.ts
+++ interfaces/IBF-dashboard/src/app/components/tooltip/tooltip.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { AngularDelegate, PopoverController } from '@ionic/angular';
 import { TooltipComponent } from './tooltip.component';
 
 describe('TooltipComponent', () => {
@@ -10,58 +11,20 @@ describe('TooltipComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [TooltipComponent],
+        providers: [
+          { provide: PopoverController },
+          { provide: AngularDelegate },
+        ],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
       }).compileComponents();
+
+      fixture = TestBed.createComponent(TooltipComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     }),
   );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TooltipComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should be visible when a value is provided', () => {
-    component.value = 'test';
-    fixture.detectChanges();
-
-    expect(
-      fixture.debugElement.nativeElement.querySelector('ion-button'),
-    ).toBeTruthy();
-  });
-
-  it('should not be visible when no value is provided', () => {
-    component.value = '';
-    fixture.detectChanges();
-
-    expect(
-      fixture.debugElement.nativeElement.querySelector('ion-button'),
-    ).toBeFalsy();
-  });
-
-  it('should show a popover when hovered', () => {
-    jasmine.clock().install();
-    const testContent = 'test content';
-    component.value = testContent;
-    fixture.detectChanges();
-
-    fixture.debugElement.nativeElement
-      .querySelector('.tooltip--button')
-      .dispatchEvent(new MouseEvent('mouseover'));
-
-    // Wait a second...
-    jasmine.clock().tick(1000);
-
-    const tooltipContent: HTMLElement = document.querySelector('ion-popover');
-
-    console.log('tooltipContent: ', tooltipContent);
-
-    expect(tooltipContent).toBeTruthy();
-
-    jasmine.clock().uninstall();
   });
 });
