f0ac543261c2a4bfe4519b623fc5dd25bafc9894
jannisvisser <jannisvisser@redcross.nl>
2023-09-25 15:40:37 +0200

    fix: test

--- interfaces/IBF-dashboard/src/app/services/map-view.service.spec.ts
+++ interfaces/IBF-dashboard/src/app/services/map-view.service.spec.ts
@@ -1,11 +1,15 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MapViewService } from './map-view.service';
 
 describe('MapViewService', () => {
   let service: MapViewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    });
     service = TestBed.inject(MapViewService);
   });
 
