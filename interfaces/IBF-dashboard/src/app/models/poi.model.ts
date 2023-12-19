77d98be1cf8f11134b0b1412923468046492d793
jannisvisser <jannisvisser@redcross.nl>
2023-12-19 16:15:44 +0100

    move threshold to right AB#25371

--- interfaces/IBF-dashboard/src/app/models/poi.model.ts
+++ interfaces/IBF-dashboard/src/app/models/poi.model.ts
@@ -8,6 +8,7 @@ export class Station {
   forecastLevel: number;
   eapAlertClass: string;
   forecastReturnPeriod: number;
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
   dynamicData?: any;
 }
 
