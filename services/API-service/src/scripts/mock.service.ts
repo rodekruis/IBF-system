87253dd38c85c6eeb9ee1b92b66f5f43d3d36de4
arsforza <asforza@redcross.nl>
2024-03-14 19:36:45 +0100

    fix: add all api calls AB#26938

--- services/API-service/src/scripts/mock.service.ts
+++ services/API-service/src/scripts/mock.service.ts
@@ -20,6 +20,12 @@ import {
   MockFlashFloodsScenario,
   MockFloodsScenario,
 } from './mock.controller';
+import { In, Repository } from 'typeorm';
+import { EventPlaceCodeEntity } from '../api/event/event-place-code.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AdminAreaDynamicDataEntity } from '../api/admin-area-dynamic-data/admin-area-dynamic-data.entity';
+import { EapActionStatusEntity } from '../api/eap-actions/eap-action-status.entity';
+import { TriggerPerLeadTime } from '../api/event/trigger-per-lead-time.entity';
 
 class Scenario {
   scenarioName: string;
@@ -29,6 +35,15 @@ class Scenario {
 
 @Injectable()
 export class MockService {
+  @InjectRepository(EventPlaceCodeEntity)
+  private readonly eventPlaceCodeRepo: Repository<EventPlaceCodeEntity>;
+  @InjectRepository(TriggerPerLeadTime)
+  private readonly triggerPerLeadTimeRepo: Repository<TriggerPerLeadTime>;
+  @InjectRepository(AdminAreaDynamicDataEntity)
+  private readonly adminAreaDynamicDataRepo: Repository<AdminAreaDynamicDataEntity>;
+  @InjectRepository(EapActionStatusEntity)
+  private readonly eapActionStatusRepo: Repository<EapActionStatusEntity>;
+
   constructor(
     private metadataService: MetadataService,
     private adminAreaDynamicDataService: AdminAreaDynamicDataService,
@@ -47,8 +62,7 @@ export class MockService {
     defaultScenario: boolean,
   ) {
     if (mockBody.removeEvents) {
-      console.log('mockBody.removeEvents: ', mockBody.removeEvents);
-      // this.removeEvents()
+      await this.removeEvents(mockBody.countryCodeISO3, disasterType);
     }
 
     const selectedCountry = countries.find((country): any => {
@@ -107,6 +121,7 @@ export class MockService {
         const triggersPerLeadTime = this.getFile(
           `./src/scripts/mock-data/${disasterType}/${mockBody.countryCodeISO3}/${scenario.scenarioName}/${event.eventName}/triggers-per-leadtime.json`,
         );
+
         await this.eventService.uploadTriggerPerLeadTime({
           countryCodeISO3: mockBody.countryCodeISO3,
           triggersPerLeadTime,
@@ -116,31 +131,55 @@ export class MockService {
         });
       }
 
-      if (this.shouldMockGlofasStations(disasterType)) {
-        await this.mockGlofasStations(
-          selectedCountry,
-          DisasterType.Floods,
-          true,
-          mockBody.date,
-        );
+      if (this.shouldMockRasterFile(disasterType)) {
+        this.mockRasterFile(selectedCountry, disasterType, true);
       }
 
-      if (disasterType === DisasterType.FlashFloods) {
-        // TODO: the below methods are now duplidated between mock.service and scrpts.service
-        // TODO: the below methods still assume hard-coded leadTimes and is not flexible
+      if (this.shouldMockExposedAssets(disasterType)) {
         await this.mockExposedAssets(
           selectedCountry.countryCodeISO3,
           true, // TODO: assume triggered, no-trigger for now done via old endpoint
           mockBody.date,
         );
+      }
+
+      if (this.shouldMockDynamicPointData(disasterType)) {
         await this.mockDynamicPointData(
           selectedCountry.countryCodeISO3,
           DisasterType.FlashFloods,
           mockBody.date,
         );
-
-        // TODO: raster-file
       }
+
+      if (this.shouldMockTyphoonTrack(disasterType)) {
+        console.log('mockTyphoonTrack not implemented yet');
+        // await this.mockTyphoonTrack()
+      }
+
+      if (this.shouldMockMapImageFile(mockBody.countryCodeISO3)) {
+        this.mockMapImageFile(
+          mockBody.countryCodeISO3,
+          disasterType,
+          true,
+          event.eventName,
+        );
+      }
+
+      if (this.shouldMockGlofasStations(disasterType)) {
+        await this.mockGlofasStations(
+          selectedCountry,
+          DisasterType.Floods,
+          mockBody.date,
+          scenario.events,
+        );
+      }
+
+      // if (disasterType === DisasterType.FlashFloods) {
+      //   // TODO: the below methods are now duplidated between mock.service and scrpts.service
+      //   // TODO: the below methods still assume hard-coded leadTimes and is not flexible
+
+      //   // TODO: raster-file
+      // }
     }
   }
 
@@ -207,14 +246,23 @@ export class MockService {
   private async mockGlofasStations(
     selectedCountry,
     disasterType: DisasterType,
-    triggered: boolean,
     date: Date,
+    events: { eventName: string; leadTime: LeadTime }[],
   ) {
-    const stations = this.getFile(
-      `./src/api/point-data/dto/example/glofas-stations/glofas-stations-${
-        selectedCountry.countryCodeISO3
-      }${triggered ? '-triggered' : ''}.json`,
+    const triggeredStations = this.getFile(
+      `./src/api/point-data/dto/example/glofas-stations/glofas-stations-${selectedCountry.countryCodeISO3}-triggered.json`,
+    ).filter((station) =>
+      events.map((e) => e.eventName).includes(station.stationCode),
     );
+    console.log('triggeredStations: ', triggeredStations);
+
+    const nonTriggeredStations = this.getFile(
+      `./src/api/point-data/dto/example/glofas-stations/glofas-stations-${selectedCountry.countryCodeISO3}.json`,
+    ).filter(
+      (station) =>
+        !events.map((e) => e.eventName).includes(station.stationCode),
+    );
+    console.log('nonTriggeredStations: ', nonTriggeredStations);
 
     for (const activeLeadTime of selectedCountry.countryDisasterSettings.find(
       (s) => s.disasterType === disasterType,
@@ -224,7 +272,7 @@ export class MockService {
       );
       await this.glofasStationService.uploadTriggerDataPerStation({
         countryCodeISO3: selectedCountry.countryCodeISO3,
-        stationForecasts: stations,
+        stationForecasts: [...triggeredStations, ...nonTriggeredStations],
         leadTime: activeLeadTime as LeadTime,
         date,
       });
@@ -332,11 +380,156 @@ export class MockService {
     }
   }
 
+  private async mockRasterFile(
+    selectedCountry,
+    disasterType: DisasterType,
+    triggered: boolean,
+  ) {
+    for await (const leadTime of selectedCountry.countryDisasterSettings.find(
+      (s) => s.disasterType === disasterType,
+    ).activeLeadTimes) {
+      console.log(
+        `Seeding disaster extent raster file for country: ${selectedCountry.countryCodeISO3} for leadtime: ${leadTime}`,
+      );
+
+      let sourceFileName, destFileName;
+      if (disasterType === DisasterType.Floods) {
+        sourceFileName = `flood_extent_${leadTime}_${selectedCountry.countryCodeISO3}.tif`;
+        destFileName = sourceFileName;
+      } else if (disasterType === DisasterType.HeavyRain) {
+        // Use 3-day mock for every lead-time
+        sourceFileName = `rainfall_extent_3-day_${
+          selectedCountry.countryCodeISO3
+        }${triggered ? '-triggered' : ''}.tif`;
+        if (selectedCountry.countryCodeISO3 === 'EGY') {
+          destFileName = `rain_rp_${leadTime}_${selectedCountry.countryCodeISO3}.tif`;
+        } else if (selectedCountry.countryCodeISO3 === 'UGA') {
+          destFileName = `rainfall_extent_${leadTime}_${selectedCountry.countryCodeISO3}.tif`;
+        }
+      } else if (disasterType === DisasterType.Drought) {
+        // Use 0-month mock for every lead-time
+        sourceFileName = `rainfall_forecast_0-month_${
+          selectedCountry.countryCodeISO3
+        }${triggered ? '-triggered' : ''}.tif`;
+        destFileName = `rain_rp_${leadTime}_${selectedCountry.countryCodeISO3}.tif`;
+      } else if (disasterType === DisasterType.FlashFloods) {
+        if (leadTime === LeadTime.hour24 || leadTime === LeadTime.hour6) {
+          sourceFileName = `flood_extent_${leadTime}_${selectedCountry.countryCodeISO3}.tif`;
+        } else {
+          continue;
+        }
+        destFileName = sourceFileName;
+      }
+
+      let file;
+      try {
+        file = fs.readFileSync(
+          `./geoserver-volume/raster-files/mock-output/${sourceFileName}`,
+        );
+      } catch (error) {
+        console.log(`ERROR: ${sourceFileName} not found.`);
+        return;
+      }
+
+      const dataObject = {
+        originalname: destFileName,
+        buffer: file,
+      };
+      await this.adminAreaDynamicDataService.postRaster(
+        dataObject,
+        disasterType,
+      );
+    }
+  }
+
+  private async mockMapImageFile(
+    countryCodeISO3: string,
+    disasterType: DisasterType,
+    triggered: boolean,
+    eventName: string,
+  ) {
+    if (!triggered) {
+      return;
+    }
+    console.log(`Seeding event map image country: ${countryCodeISO3}`);
+
+    // const eventName = this.getEventName(disasterType) || 'no-name';
+    const filename = `${countryCodeISO3}_${disasterType}_${eventName}_map-image.png`;
+    const file = fs.readFileSync(
+      `./geoserver-volume/raster-files/mock-output/${filename}`,
+    );
+    const dataObject = {
+      originalname: filename,
+      buffer: file,
+    };
+    await this.eventService.postEventMapImage(
+      countryCodeISO3,
+      disasterType,
+      eventName,
+      dataObject,
+    );
+  }
+
+  private async removeEvents(
+    countryCodeISO3: string,
+    disasterType: DisasterType,
+  ) {
+    const countryAdminAreaIds = await this.eventService.getCountryAdminAreaIds(
+      countryCodeISO3,
+    );
+
+    const allCountryEvents = await this.eventPlaceCodeRepo.find({
+      relations: ['eapActionStatuses', 'adminArea'],
+      where: {
+        adminArea: In(countryAdminAreaIds),
+        disasterType,
+      },
+    });
+    await this.triggerPerLeadTimeRepo.delete({
+      countryCodeISO3,
+      disasterType,
+    });
+    await this.adminAreaDynamicDataRepo.delete({
+      countryCodeISO3,
+      disasterType,
+    });
+
+    for (const event of allCountryEvents) {
+      await this.eapActionStatusRepo.remove(event.eapActionStatuses);
+    }
+    await this.eventPlaceCodeRepo.remove(allCountryEvents);
+  }
+
   private shouldMockGlofasStations(disasterType: DisasterType): boolean {
     return disasterType === DisasterType.Floods;
   }
 
   private shouldMockTriggerPerLeadTime(disasterType: DisasterType): boolean {
     return disasterType === DisasterType.Floods;
+  }
+
+  private shouldMockRasterFile(disasterType: DisasterType): boolean {
+    return [
+      DisasterType.Floods,
+      DisasterType.HeavyRain,
+      DisasterType.Drought,
+      DisasterType.FlashFloods,
+    ].includes(disasterType);
+  }
+
+  private shouldMockExposedAssets(disasterType: DisasterType): boolean {
+    return disasterType === DisasterType.FlashFloods;
+  }
+
+  private shouldMockDynamicPointData(disasterType: DisasterType): boolean {
+    return disasterType === DisasterType.FlashFloods;
+  }
+
+  private shouldMockTyphoonTrack(disasterType: DisasterType): boolean {
+    return disasterType === DisasterType.Typhoon;
+  }
+
+  private shouldMockMapImageFile(countryCodeISO3: string): boolean {
+    return countryCodeISO3 === 'SSD';
   }
 }
