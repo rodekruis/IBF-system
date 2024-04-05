623a7f330277fe391e24daa99a41199a89608858
arsforza <asforza@redcross.nl>
2024-04-05 14:53:09 +0200

    refactor: move event eapAlertClass to be AB#27284

--- services/API-service/src/shared/data.model.ts
+++ services/API-service/src/shared/data.model.ts
@@ -58,8 +58,14 @@ export class TriggeredArea {
 export class DisasterSpecificProperties {
   typhoonLandfall?: boolean;
   typhoonNoLandfallYet?: boolean;
+  eapAlertClass?: {
+    key: string;
+    label: string;
+    color: string;
+    value: number;
+    textColor?: string;
+  };
 }
-
 export class EventSummaryCountry {
   @ApiProperty({ example: 'UGA' })
   public countryCodeISO3: string;
