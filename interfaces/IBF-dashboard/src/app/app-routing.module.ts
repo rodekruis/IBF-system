0d06a69a1000e077525a95fe4433d3229b4191b0
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2024-07-15 15:53:12 +0200

    build: ng-15

--- interfaces/IBF-dashboard/src/app/app-routing.module.ts
+++ interfaces/IBF-dashboard/src/app/app-routing.module.ts
@@ -21,9 +21,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-      relativeLinkResolution: 'legacy',
-    }),
+    preloadingStrategy: PreloadAllModules
+}),
   ],
   exports: [RouterModule],
 })
