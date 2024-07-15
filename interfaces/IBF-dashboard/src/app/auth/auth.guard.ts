f573215a926dcc7cf45375e0146c9549f4057d15
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2024-07-15 16:03:09 +0200

    build: ng-16

--- interfaces/IBF-dashboard/src/app/auth/auth.guard.ts
+++ interfaces/IBF-dashboard/src/app/auth/auth.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router, private authService: AuthService) {}
 
   canActivate(
