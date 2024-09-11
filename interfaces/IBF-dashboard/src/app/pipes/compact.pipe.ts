7a809441254fa3f90ca4fd884513bf7e7b99eb51
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2024-09-11 15:25:19 +0200

    fix: prefix for compact numbers

--- interfaces/IBF-dashboard/src/app/pipes/compact.pipe.ts
+++ interfaces/IBF-dashboard/src/app/pipes/compact.pipe.ts
@@ -8,17 +8,49 @@ import { NumberFormat } from '../types/indicator-group';
 export class CompactPipe implements PipeTransform {
   constructor(@Inject(LOCALE_ID) private locale: string) {}
 
+  /*
+   * 0 becomes 0
+   * 2 becomes < 10
+   * 12 becomes < 20
+   * 20 becomes 20
+   * 56 becomes 60
+   * 297 becomes 300
+   * 462 becomes 460
+   * 1000 becomes 1K
+   * 4200 becomes 4.2K
+   * 225305 becomes 230K
+   * 79136946 becomes 79M
+   * negative numbers become 0
+   */
   transform(value: number, format: NumberFormat = NumberFormat.decimal0) {
+    if (value == null || isNaN(value)) {
+      return '';
+    }
+
     const style = format === NumberFormat.perc ? 'percent' : 'decimal';
-    const min = format === NumberFormat.perc ? 0.1 : 10;
-    const maximumSignificantDigits = format === NumberFormat.perc ? 2 : 1;
+    const maximumSignificantDigits = value > 100 ? 2 : 1;
 
-    value = value > 0 ? Math.max(min, value) : 0;
+    let min = 0;
+    let prefix = '';
 
-    return new Intl.NumberFormat(this.locale, {
+    if (format !== NumberFormat.perc) {
+      if (value > 10) {
+        min = 20;
+      } else if (value > 0) {
+        min = 10;
+      }
+
+      if (value > 0 && value < 20) {
+        prefix = '< ';
+      }
+    }
+
+    value = value > 0 ? Math.max(value, min) : 0;
+
+    return `${prefix}${new Intl.NumberFormat(this.locale, {
       maximumSignificantDigits,
       style,
       notation: 'compact',
-    }).format(value);
+    }).format(value)}`;
   }
 }
