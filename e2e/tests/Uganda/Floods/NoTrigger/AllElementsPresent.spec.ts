3d29df2362dff49196c48e4102f01310a59e877a
piotrk39 <piotrk39@gmail.com>
2024-09-26 08:41:56 +0200

    Add flodds no-trigger reset flow

--- e2e/tests/Uganda/Floods/NoTrigger/AllElementsPresent.spec.ts
+++ e2e/tests/Uganda/Floods/NoTrigger/AllElementsPresent.spec.ts
@@ -1,8 +1,10 @@
 import { test } from '@playwright/test';
 
+import { FloodsScenario } from '../../../../../services/API-service/src/scripts/enum/mock-scenario.enum';
 import users from '../../../../../services/API-service/src/scripts/json/users.json';
 import {
   getAccessToken,
+  mockFloods,
   resetDB,
 } from '../../../../../services/API-service/test/helpers/utility.helper';
 import LoginPage from '../../../../Pages/LoginPage';
@@ -16,11 +18,12 @@ test.beforeEach(async ({ page }) => {
 
   accessToken = await getAccessToken();
   await resetDB(accessToken);
+  await mockFloods(FloodsScenario.NoTrigger, 'UGA', accessToken);
 
   await page.goto('/');
   await loginPage.login(admin?.email, admin?.password);
 });
 
-test('All elements are present', async ({ page }) => {
+test('[30499] All Dashboard elements are present', async ({ page }) => {
   await page.waitForURL((url) => url.pathname === '/');
 });
