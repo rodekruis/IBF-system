943718e79cb17218e22d649663243b46ee06337f
piotrk39 <piotrk39@gmail.com>
2024-10-02 13:30:31 +0200

    Add all UserState locators and assertions

--- e2e/Pages/UserStateComponent.ts
+++ e2e/Pages/UserStateComponent.ts
@@ -6,11 +6,21 @@ import DashboardPage from './DashboardPage';
 class UserStateComponent extends DashboardPage {
   readonly page: Page;
   readonly header: Locator;
+  readonly countryLogos: Locator;
+  readonly userLoggedInLabel: Locator;
+  readonly logOutButton: Locator;
 
   constructor(page: Page) {
     super(page);
     this.page = page;
     this.header = this.page.getByTestId('heading-display-name-span');
+    this.countryLogos = this.page
+      .getByTestId('user-state-country-logos')
+      .nth(1);
+    this.userLoggedInLabel = this.page
+      .getByTestId('user-state-display-name-label')
+      .nth(1);
+    this.logOutButton = this.page.getByTestId('user-state-logout-button');
   }
 
   async headerComponentIsVisible({ countryName }: { countryName: string }) {
@@ -18,6 +28,21 @@ class UserStateComponent extends DashboardPage {
       hasText: `IBF PORTAL ${countryName}`,
     });
     await expect(header).toBeVisible();
+  }
+
+  async allUserStateElementsAreVisible({
+    firstName,
+    lastName,
+  }: {
+    firstName: string;
+    lastName: string;
+  }) {
+    await expect(this.dashboardHomeButton).toBeVisible();
+    await expect(this.countryLogos).toBeVisible();
+    await expect(this.userLoggedInLabel).toHaveText(
+      `Logged In As:${firstName} ${lastName}`,
+    );
+    await expect(this.logOutButton).toBeVisible();
   }
 }
 
