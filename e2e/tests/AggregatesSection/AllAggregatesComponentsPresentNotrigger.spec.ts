6a5e529b867b77152ee3601315ec8ee14e4b98e5
piotrk39 <piotrk39@gmail.com>
2024-10-04 14:06:32 +0200

    Add base test

---
+++ e2e/tests/AggregatesSection/AllAggregatesComponentsPresentNotrigger.spec.ts
@@ -0,0 +1,46 @@
+import { test } from '@playwright/test';
+import AggregatesComponent from 'Pages/AggregateComponenet';
+import DashboardPage from 'Pages/DashboardPage';
+import { NoTriggerDataSet } from 'testData/testData.enum';
+
+import { FloodsScenario } from '../../../services/API-service/src/scripts/enum/mock-scenario.enum';
+import {
+  getAccessToken,
+  mockFloods,
+  resetDB,
+} from '../../../services/API-service/test/helpers/utility.helper';
+import LoginPage from '../../Pages/LoginPage';
+
+let accessToken: string;
+
+test.beforeEach(async ({ page }) => {
+  // Login
+  const loginPage = new LoginPage(page);
+
+  accessToken = await getAccessToken();
+  await resetDB(accessToken);
+  // We should maybe create one mock for all different disaster types for now we can just use floods
+  await mockFloods(
+    FloodsScenario.NoTrigger,
+    NoTriggerDataSet.CountryCode,
+    accessToken,
+  );
+
+  await page.goto('/');
+  await loginPage.login(
+    NoTriggerDataSet.UserMail,
+    NoTriggerDataSet.UserPassword,
+  );
+});
+
+test('[30588] All Aggregate elements are present in no-trigger mode', async ({
+  page,
+}) => {
+  const dashboard = new DashboardPage(page);
+  const aggregates = new AggregatesComponent(page);
+
+  // Navigate to disaster type the data was mocked for
+  await dashboard.navigateToFloodDisasterType();
+  // Assertions
+  await aggregates.aggregateComponentIsVisible();
+});
