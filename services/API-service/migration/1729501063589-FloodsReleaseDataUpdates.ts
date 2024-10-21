3d11334f0df53edd25362625b6bcc038e0636433
arsforza <asforza@redcross.nl>
2024-10-21 15:09:59 +0200

    fix: add check for leadtime not existing

--- services/API-service/migration/1729501063589-FloodsReleaseDataUpdates.ts
+++ services/API-service/migration/1729501063589-FloodsReleaseDataUpdates.ts
@@ -12,32 +12,37 @@ export class FloodsReleaseDataUpdates1729501063589
       `UPDATE "IBF-app".disaster SET "minLeadTime"='0-day' WHERE "disasterType"='floods';`,
     );
 
-    // update lead-time table
-    await queryRunner.query(
-      `INSERT INTO "IBF-app"."lead-time"
+    // update lead-time table (if not exists)
+    const leadTime0dayExists = await queryRunner.query(
+      `SELECT * FROM "IBF-app"."lead-time" WHERE "leadTimeName"='0-day';`,
+    );
+    if (!leadTime0dayExists) {
+      await queryRunner.query(
+        `INSERT INTO "IBF-app"."lead-time"
         ("leadTimeId", "leadTimeName", created)
         VALUES(uuid_generate_v4(), '0-day', now());
         `,
-    );
+      );
+    }
 
     // close open floods events ..
     // .. by 1. setting endDate to now()
     await queryRunner.query(
-      `UPDATE "IBF-app"."event-place-code" 
+      `UPDATE "IBF-app"."event-place-code"
             SET "endDate" = now()
-            WHERE "disasterType" = 'floods' 
-            AND "eventName" is null 
-            AND "adminAreaId" is not null 
+            WHERE "disasterType" = 'floods'
+            AND "eventName" is null
+            AND "adminAreaId" is not null
             AND closed = false;
           `,
     );
     // .. and 2. closing the event
     await queryRunner.query(
-      `UPDATE "IBF-app"."event-place-code" 
-            SET closed = true 
-            WHERE "disasterType" = 'floods' 
-            AND "eventName" is null 
-            AND "adminAreaId" is not null 
+      `UPDATE "IBF-app"."event-place-code"
+            SET closed = true
+            WHERE "disasterType" = 'floods'
+            AND "eventName" is null
+            AND "adminAreaId" is not null
             AND closed = false;
           `,
     );
