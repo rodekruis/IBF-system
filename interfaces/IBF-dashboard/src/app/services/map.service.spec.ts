4a3271317ddd4ccbb97564461d9a5738e9343785
arsforza <asforza@redcross.nl>
2024-10-29 10:14:25 +0100

    test: add first map service test

--- interfaces/IBF-dashboard/src/app/services/map.service.spec.ts
+++ interfaces/IBF-dashboard/src/app/services/map.service.spec.ts
@@ -7,6 +7,30 @@ import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
+import { IbfLayerLabel, IbfLayerName, IbfLayerType } from '../types/ibf-layer';
+
+const MOCK_LAYERS = [
+  {
+    type: IbfLayerType.point,
+    name: IbfLayerName.waterpointsInternal,
+    label: IbfLayerLabel.waterpoints,
+    description: 'waterpointsInternal',
+    active: true,
+    show: true,
+    viewCenter: false,
+    order: 1,
+  },
+  {
+    type: IbfLayerType.point,
+    name: IbfLayerName.redCrossBranches,
+    label: IbfLayerLabel.redCrossBranches,
+    description: 'redCrossBranches',
+    active: true,
+    show: true,
+    viewCenter: false,
+    order: 2,
+  },
+];
 
 describe('MapService', () => {
   let service: MapService;
@@ -24,5 +48,25 @@ describe('MapService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getLayerByName', () => {
+    beforeEach(() => {
+      service['layers'] = MOCK_LAYERS;
+    });
+
+    it('should get a layer from its name', () => {
+      const layerName = IbfLayerName.waterpointsInternal;
+
+      const expected = MOCK_LAYERS[0];
+
+      expect(service.getLayerByName(layerName)).toEqual(expected);
+    });
+
+    it('should be undefined if no layer has the provided name', () => {
+      const layerName = IbfLayerName.gauges;
+
+      expect(service.getLayerByName(layerName)).toBeUndefined();
+    });
   });
 });
