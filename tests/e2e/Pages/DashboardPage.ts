422ba40b11323a96456b79d83b88e98eb07aefe4
piotrk39 <piotrk39@gmail.com>
2025-01-12 23:13:58 +0100

    remove comment

--- tests/e2e/Pages/DashboardPage.ts
+++ tests/e2e/Pages/DashboardPage.ts
@@ -77,7 +77,6 @@ class DashboardPage {
 
   async waitForPageToBeLoadedAndStable() {
     await this.page.waitForLoadState('domcontentloaded');
-    // await this.page.waitForLoadState('networkidle');
     await this.page.waitForTimeout(500);
   }
 }
