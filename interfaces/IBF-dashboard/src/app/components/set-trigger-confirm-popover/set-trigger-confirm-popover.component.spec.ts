10737be903960ce2353285b4ecb628e15226e6e5
jannisvisser <jannisvisser@redcross.nl>
2025-03-05 11:22:30 +0100

    fix: basic setup unit tests

--- interfaces/IBF-dashboard/src/app/components/set-trigger-confirm-popover/set-trigger-confirm-popover.component.spec.ts
+++ interfaces/IBF-dashboard/src/app/components/set-trigger-confirm-popover/set-trigger-confirm-popover.component.spec.ts
@@ -1,11 +1,33 @@
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { IonicModule, PopoverController } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { SetTriggerConfirmPopoverComponent } from 'src/app/components/set-trigger-confirm-popover/set-trigger-confirm-popover.component';
 
 describe('SetTriggerConfirmPopoverComponent', () => {
   let component: SetTriggerConfirmPopoverComponent;
   let fixture: ComponentFixture<SetTriggerConfirmPopoverComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SetTriggerConfirmPopoverComponent],
+      imports: [
+        IonicModule.forRoot(),
+        TranslateModule.forRoot(),
+        RouterModule.forRoot([]),
+      ],
+      providers: [
+        PopoverController,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SetTriggerConfirmPopoverComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
