e27be11713724cfbe2dd0ba5c5179a49f6de7e8f
jannisvisser <jannisvisser@redcross.nl>
2025-03-05 11:22:30 +0100

    fix: basic setup unit tests

--- interfaces/IBF-dashboard/src/app/components/set-trigger-popover/set-trigger-popover.component.spec.ts
+++ interfaces/IBF-dashboard/src/app/components/set-trigger-popover/set-trigger-popover.component.spec.ts
@@ -1,11 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule, PopoverController } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { SetTriggerPopoverComponent } from 'src/app/components/set-trigger-popover/set-trigger-popover.component';
 
 describe('SetTriggerPopoverComponent', () => {
   let component: SetTriggerPopoverComponent;
   let fixture: ComponentFixture<SetTriggerPopoverComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SetTriggerPopoverComponent],
+      imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
+      providers: [PopoverController],
+    }).compileComponents();
     fixture = TestBed.createComponent(SetTriggerPopoverComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
