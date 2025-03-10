fc046af9e7b3382f75f5a7339364f04ae5bb3b17
jannisvisser <jannisvisser@redcross.nl>
2025-03-10 13:49:23 +0100

    test: remove outdated test/assertion for disabled timeline

--- tests/e2e/tests/tests.spec.ts
+++ tests/e2e/tests/tests.spec.ts
@@ -38,7 +38,6 @@ import MapComponentLayersDefault from './MapComponent/MapComponentLayersDefault'
 import MapComponentLayersVisible from './MapComponent/MapComponentLayersVisible';
 import MapComponentTriggerLayer from './MapComponent/MapComponentTriggerLayer';
 import MapComponentVisible from './MapComponent/MapComponentVisible';
-import TimelineComponentDisabled from './TimelineComponent/TimelineComponentDisabled';
 import TimelineComponentNotClickable from './TimelineComponent/TimelineComponentNotClickable';
 import TimelineComponentVisible from './TimelineComponent/TimelineComponentVisible';
 import UserStateComponentLogout from './UserStateComponent/UserStateComponentLogout';
@@ -153,7 +152,6 @@ test.describe('E2E Tests', () => {
 
       test.describe('TimelineComponent', () => {
         TimelineComponentVisible(pages, components, dataset);
-        TimelineComponentDisabled(pages, components, dataset);
 
         if (scenario === 'trigger') {
           // REFACTOR
