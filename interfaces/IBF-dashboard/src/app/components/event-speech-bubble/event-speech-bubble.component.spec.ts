d93d4d67ec46375b1e30f74ad1e56f571136b655
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-03-20 18:44:54 +0100

    test: fix set trigger popover unit tests

--- interfaces/IBF-dashboard/src/app/components/event-speech-bubble/event-speech-bubble.component.spec.ts
+++ interfaces/IBF-dashboard/src/app/components/event-speech-bubble/event-speech-bubble.component.spec.ts
@@ -9,7 +9,11 @@ import { IonicModule, PopoverController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { EventSpeechBubbleComponent } from 'src/app/components/event-speech-bubble/event-speech-bubble.component';
 import { MOCK_ALERT_AREAS } from 'src/app/mocks/alert-areas.mock';
+import { MOCK_COUNTRY } from 'src/app/mocks/country.mock';
+import { MOCK_COUNTRYDISASTERSETTINGS } from 'src/app/mocks/country-disaster-settings.mock';
+import { MOCK_DISASTERTYPE } from 'src/app/mocks/disaster-type.mock';
 import { MOCK_EVENT_STATE } from 'src/app/mocks/event-state.mock';
+import { MOCK_INDICATOR } from 'src/app/mocks/indicator.mock';
 import { CountryDisasterSettings } from 'src/app/models/country.model';
 import { UserRole } from 'src/app/models/user/user-role.enum';
 
@@ -60,12 +64,19 @@ fdescribe('EventSpeechBubbleComponent', () => {
       label: 'Test Source',
       url: 'http://test.com',
     };
-    component.countryDisasterSettings.eapLink = 'http://test.com';
-    component.adminAreaLabelPlural = 'Districts';
+    component.countryDisasterSettings.eapLink =
+      MOCK_COUNTRYDISASTERSETTINGS.eapLink;
+    component.adminAreaLabelPlural = MOCK_COUNTRY.adminRegionLabels['2'].plural;
     component.areas = MOCK_ALERT_AREAS;
+    component.mainExposureIndicatorNumberFormat =
+      MOCK_INDICATOR.numberFormatMap;
+    component.countryCodeISO3 = MOCK_COUNTRY.countryCodeISO3;
+    component.disasterType = MOCK_DISASTERTYPE;
 
     // Spy on the hasSetTriggerPermission method
-    // spyOn(component, 'hasSetTriggerPermission').and.returnValue(true);
+    spyOn(component, 'hasSetTriggerPermission').and.callFake(
+      () => component.userRole === UserRole.LocalAdmin,
+    );
 
     fixture.detectChanges();
   }));
@@ -75,13 +86,13 @@ fdescribe('EventSpeechBubbleComponent', () => {
   });
 
   describe('hasSetTriggerPermission', () => {
-    it('return true for right userRole', async () => {
+    it('return true for right userRole', () => {
       component.userRole = UserRole.LocalAdmin;
       const result = component.hasSetTriggerPermission();
       expect(result).toBeTrue();
     });
 
-    it('return false for wrong userRole', async () => {
+    it('return false for wrong userRole', () => {
       component.userRole = UserRole.Operator;
       const result = component.hasSetTriggerPermission();
       expect(result).toBeFalse();
@@ -92,7 +103,6 @@ fdescribe('EventSpeechBubbleComponent', () => {
     it('should open set trigger popover when openSetTriggerPopover is called', async () => {
       // Arrange
       component.userRole = UserRole.LocalAdmin;
-      const expectedHasSetTriggerPermission = true;
 
       // Act
       await component.openSetTriggerPopover();
@@ -106,7 +116,11 @@ fdescribe('EventSpeechBubbleComponent', () => {
             eapLink: component.countryDisasterSettings.eapLink,
             adminAreaLabelPlural: component.adminAreaLabelPlural,
             areas: component.areas,
-            hasSetTriggerPermission: expectedHasSetTriggerPermission,
+            mainExposureIndicatorNumberFormat:
+              component.mainExposureIndicatorNumberFormat,
+            hasSetTriggerPermission: component.hasSetTriggerPermission(),
+            countryCodeISO3: component.countryCodeISO3,
+            disasterType: component.disasterType.disasterType,
           }),
           showBackdrop: true,
         }),
@@ -116,7 +130,6 @@ fdescribe('EventSpeechBubbleComponent', () => {
     it('should open no-access set trigger popover when openSetTriggerPopover is called without right userRole', async () => {
       // Arrange
       component.userRole = UserRole.Operator;
-      const expectedHasSetTriggerPermission = false;
 
       // Act
       await component.openSetTriggerPopover();
@@ -130,7 +143,11 @@ fdescribe('EventSpeechBubbleComponent', () => {
             eapLink: component.countryDisasterSettings.eapLink,
             adminAreaLabelPlural: component.adminAreaLabelPlural,
             areas: component.areas,
-            hasSetTriggerPermission: expectedHasSetTriggerPermission,
+            mainExposureIndicatorNumberFormat:
+              component.mainExposureIndicatorNumberFormat,
+            hasSetTriggerPermission: component.hasSetTriggerPermission(),
+            countryCodeISO3: component.countryCodeISO3,
+            disasterType: component.disasterType.disasterType,
           }),
           showBackdrop: true,
         }),
