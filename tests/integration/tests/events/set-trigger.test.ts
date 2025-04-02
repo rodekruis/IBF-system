ca576802569f0fb94a33753577d0e65936cfdcbe
jannisvisser <jannisvisser@redcross.nl>
2025-04-02 13:27:12 +0200

    test: add full drought season cycle integration test AB#34701

--- tests/integration/tests/events/set-trigger.test.ts
+++ tests/integration/tests/events/set-trigger.test.ts
@@ -10,7 +10,7 @@ import {
   sendNotification,
 } from '../../helpers/utility.helper';
 
-const countryCodeISO3 = 'UGA';
+const countryCodeISO3 = 'LSO';
 const disasterType = DisasterType.Drought;
 
 describe('set trigger', () => {
@@ -23,12 +23,12 @@ describe('set trigger', () => {
 
   it('should successfully set trigger and change alertLevel from warning to trigger and send email', async () => {
     // Arrange
-    const dateJanuary = new Date(new Date().getFullYear(), 0, 2);
+    const dateAugust = new Date(new Date().getFullYear(), 7, 2);
     await mock(
       DroughtScenario.Warning,
       disasterType,
       countryCodeISO3,
-      dateJanuary,
+      dateAugust,
       accessToken,
     );
 
@@ -85,5 +85,106 @@ describe('set trigger', () => {
     const emailContent = sendNotificationResult.body.activeEvents.email;
     expect(emailContent).toContain('Set by:');
     expect(emailContent).not.toContain('Forecast source:');
+  });
+
+  it('should correctly update drought events data throughout a full season cycle', async () => {
+    // july warning 3-month > assert basic working
+    let removeEvents = true; // set to true only for first mock to reset from previous test
+    const dateJuly = new Date(new Date().getFullYear(), 6, 2);
+    await mock(
+      DroughtScenario.Warning,
+      disasterType,
+      countryCodeISO3,
+      dateJuly,
+      accessToken,
+      removeEvents,
+    );
+    const eventsJuly = await getEventsSummary(
+      countryCodeISO3,
+      disasterType,
+      accessToken,
+    );
+    expect(eventsJuly.body[0].alertLevel).toBe(AlertLevel.WARNING);
+
+    // august no-alert 2-month > assert downgrade to no-alert
+    removeEvents = false; // from now on do not overwrite events
+    const dateAugust = new Date(new Date().getFullYear(), 7, 2);
+    await mock(
+      DroughtScenario.NoTrigger,
+      disasterType,
+      countryCodeISO3,
+      dateAugust,
+      accessToken,
+      removeEvents,
+    );
+    const eventsAug = await getEventsSummary(
+      countryCodeISO3,
+      disasterType,
+      accessToken,
+    );
+    expect(eventsAug.body.length).toBe(0);
+
+    // september warning 1-month > bring back to warning and test set-trigger
+    const dateSept = new Date(new Date().getFullYear(), 8, 2);
+    await mock(
+      DroughtScenario.Warning,
+      disasterType,
+      countryCodeISO3,
+      dateSept,
+      accessToken,
+      removeEvents,
+    );
+    const eventsSeptBefore = await getEventsSummary(
+      countryCodeISO3,
+      disasterType,
+      accessToken,
+    );
+    await postSetTrigger(
+      eventsSeptBefore.body[0].alertAreas.map((area) => area.eventPlaceCodeId),
+      countryCodeISO3,
+      disasterType,
+      true,
+      accessToken,
+    );
+    const eventsSeptAfter = await getEventsSummary(
+      countryCodeISO3,
+      disasterType,
+      accessToken,
+    );
+    expect(eventsSeptAfter.body[0].alertLevel).toBe(AlertLevel.TRIGGER);
+
+    // october warning 0-month > assert "set" trigger remains trigger despite warning pipeline forecast
+    const dateOct = new Date(new Date().getFullYear(), 9, 2);
+    await mock(
+      DroughtScenario.Warning, // This must be warning, according to the requirement of no update during season
+      disasterType,
+      countryCodeISO3,
+      dateOct,
+      accessToken,
+      removeEvents,
+    );
+    const eventsOct = await getEventsSummary(
+      countryCodeISO3,
+      disasterType,
+      accessToken,
+    );
+    expect(eventsOct.body[0].alertLevel).toBe(AlertLevel.TRIGGER);
+
+    // april no-alert 6-month > assert reset to no alert at end of season
+    const dateApril = new Date(new Date().getFullYear() + 1, 3, 2);
+    await mock(
+      DroughtScenario.NoTrigger,
+      disasterType,
+      countryCodeISO3,
+      dateApril,
+      accessToken,
+      removeEvents,
+    );
+    const eventsApr = await getEventsSummary(
+      countryCodeISO3,
+      disasterType,
+      accessToken,
+    );
+    expect(eventsApr.body.length).toBe(0);
   });
 });
