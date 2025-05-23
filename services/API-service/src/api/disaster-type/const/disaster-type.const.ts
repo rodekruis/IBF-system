122447da68fe3591a8084f5e905d86f2275abe74
jannisvisser <jannisvisser@redcross.nl>
2025-05-23 16:15:04 +0200

    feat: disable event-areas for drought again AB#36246

--- services/API-service/src/api/disaster-type/const/disaster-type.const.ts
+++ services/API-service/src/api/disaster-type/const/disaster-type.const.ts
@@ -2,6 +2,6 @@ import { DisasterType } from '../disaster-type.enum';
 
 export const EVENT_AREA_DISASTER_TYPES = [
   DisasterType.Floods,
-  DisasterType.Drought,
+  // DisasterType.Drought, // REFACTOR: too slow for current on-the-fly calculation. Refactor would be to create an events table and calculate union only once in /events/process.
   DisasterType.FlashFloods,
 ];
--- tests/integration/helpers/API-service/const/disaster-type.const.ts
+++ tests/integration/helpers/API-service/const/disaster-type.const.ts
@@ -2,6 +2,5 @@ import { DisasterType } from '../enum/disaster-type.enum';
 
 export const EVENT_AREA_DISASTER_TYPES = [
   DisasterType.Floods,
-  DisasterType.Drought,
   DisasterType.FlashFloods,
 ];
