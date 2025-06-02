9da6e31cc5597e0dfda8753a211b81a07b3a4c89
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-06-02 18:12:55 +0200

    test: reword and regroup aggregates and delete tests

--- tests/integration/tests/all.test.ts
+++ tests/integration/tests/all.test.ts
@@ -1,8 +1,8 @@
 import { reset } from '../helpers/utility.helper';
 import adminAreaDataTests from './admin-area-data/admin-area-data.test';
 import adminAreaTests from './admin-areas/admin-areas.test';
+import { adminAreaDeleteTests } from './admin-areas/admin-areas-delete.test';
 import adminAreaAggregatesTests from './admin-areas/aggregates.test';
-import { adminAreaDeleteTests } from './admin-areas/delete-admin-areas.test';
 import eventAdminAreaTests from './admin-areas/event-admin-areas.test';
 import communityNotificationTests from './community-notification/community-notification.test';
 import createCountryTests from './country/create-country.test';
