65a10e0fdc47db9202cc196cc3636f933a5b9f9a
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-06-11 11:31:48 +0200

    refactor: update set env vars script

--- interfaces/IBF-dashboard/_set-env-variabes.js
+++ interfaces/IBF-dashboard/_set-env-variabes.js
@@ -1,15 +1,29 @@
-import { writeFile } from 'fs';
+import { writeFile, readFileSync } from 'fs';
 import { config } from 'dotenv';
 
-// Load environment-variables from .env file (if available)
 config();
 
-import configFileTemplate from './src/environments/environment.template.ts';
+const templatePath = './src/environments/environment.template.ts';
 const targetPath = `./src/environments/environment.${process.env.NG_CONFIGURATION}.ts`;
 
-writeFile(targetPath, configFileTemplate, (err) => {
-  if (err) {
-    console.log(err);
+let template = readFileSync(templatePath, 'utf8');
+
+template = template.replace(
+  /process\.env\[['"]([^'"]+)['"]\]/g,
+  (_, varName) => {
+    const value = process.env[varName];
+
+    if (['null', 'undefined', 'true', 'false'].includes(value)) return value;
+
+    return `'${value}'`;
+  },
+);
+
+console.log(`Writing environment file to: ${targetPath}`);
+
+writeFile(targetPath, template, (error) => {
+  if (error) {
+    console.error(`Error writing environment file: ${error}`);
   }
 
   console.log(`Output generated at ${targetPath}`);
