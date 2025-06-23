4d715e9774e9224a65d7454aa24a806b5ccbc4a7
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-06-24 00:34:08 +0200

    build: use common js for karma

--- interfaces/IBF-dashboard/karma.conf.cjs
+++ interfaces/IBF-dashboard/karma.conf.cjs
@@ -1,28 +1,22 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-import { join } from 'path';
-import karmaJasmine from 'karma-jasmine';
-import karmaChromeLauncher from 'karma-chrome-launcher';
-import karmaJasmineHtmlReporter from 'karma-jasmine-html-reporter';
-import karmaCoverageIstanbulReporter from 'karma-coverage-istanbul-reporter';
-import angularKarma from '@angular-devkit/build-angular/plugins/karma';
 
-export default function (config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      karmaJasmine,
-      karmaChromeLauncher,
-      karmaJasmineHtmlReporter,
-      karmaCoverageIstanbulReporter,
-      angularKarma,
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
@@ -34,4 +28,4 @@ export default function (config) {
     browsers: ['Chrome'],
     singleRun: false,
   });
-}
+};
