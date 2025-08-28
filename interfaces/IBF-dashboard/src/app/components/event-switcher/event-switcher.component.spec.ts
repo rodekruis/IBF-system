edafb32844ee031b50ee24f81afb76a60b2d647e
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-08-28 15:34:49 +0200

    test: fix unit tests

--- interfaces/IBF-dashboard/src/app/components/event-switcher/event-switcher.component.spec.ts
+++ interfaces/IBF-dashboard/src/app/components/event-switcher/event-switcher.component.spec.ts
@@ -1,30 +1,57 @@
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { provideIonicAngular } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { arrowBack, arrowForward } from 'ionicons/icons';
+import { of } from 'rxjs';
 import { EventSwitcherComponent } from 'src/app/components/event-switcher/event-switcher.component';
+import { DisasterTypeService } from 'src/app/services/disaster-type.service';
+import { EventService } from 'src/app/services/event.service';
+import { TimelineService } from 'src/app/services/timeline.service';
+import { TimelineState } from 'src/app/types/timeline-state';
 
 describe('EventSwitcherComponent', () => {
   let component: EventSwitcherComponent;
   let fixture: ComponentFixture<EventSwitcherComponent>;
+  let disasterTypeService: jasmine.SpyObj<DisasterTypeService>;
+  let timelineService: jasmine.SpyObj<TimelineService>;
+  let eventService: jasmine.SpyObj<EventService>;
 
   beforeEach(waitForAsync(() => {
     addIcons({ 'arrow-forward': arrowForward, 'arrow-back': arrowBack });
+
+    disasterTypeService = jasmine.createSpyObj<DisasterTypeService>(
+      'DisasterTypeService',
+      ['getDisasterTypeSubscription'],
+    );
+
+    disasterTypeService.getDisasterTypeSubscription.and.returnValue(of(null));
+
+    timelineService = jasmine.createSpyObj<TimelineService>('TimelineService', [
+      'getTimelineStateSubscription',
+    ]);
+
+    timelineService.getTimelineStateSubscription.and.returnValue(
+      of({ timeStepButtons: [] } as TimelineState),
+    );
+
+    eventService = jasmine.createSpyObj<EventService>(
+      'EventService',
+      ['getInitialEventStateSubscription', 'getManualEventStateSubscription'],
+      { state: { event: null, events: [] } },
+    );
+
+    eventService.getInitialEventStateSubscription.and.returnValue(of(null));
+
+    eventService.getManualEventStateSubscription.and.returnValue(of(null));
 
     TestBed.configureTestingModule({
       declarations: [EventSwitcherComponent],
       imports: [IonicModule, RouterModule.forRoot([])],
       providers: [
-        provideIonicAngular(),
-        provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting(),
+        { provide: DisasterTypeService, useValue: disasterTypeService },
+        { provide: TimelineService, useValue: timelineService },
+        { provide: EventService, useValue: eventService },
       ],
     }).compileComponents();
 
@@ -35,7 +62,7 @@ describe('EventSwitcherComponent', () => {
     fixture.detectChanges();
   }));
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
