0e56f3d4de61b8b6a0c41f0bb1e2e646cbada166
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-08-28 17:46:05 +0200

    test: e2e

--- tests/e2e/Pages/UserStateComponent.ts
+++ tests/e2e/Pages/UserStateComponent.ts
@@ -1,6 +1,6 @@
 import { expect } from '@playwright/test';
 import { Locator, Page } from 'playwright';
-import { Country, Dataset, User } from 'testData/types';
+import { Dataset, User } from 'testData/types';
 
 import DashboardPage from './DashboardPage';
 
@@ -14,31 +14,17 @@ class UserStateComponent extends DashboardPage {
   constructor(page: Page) {
     super(page);
     this.page = page;
-    this.header = this.page.getByTestId('heading-display-name-span');
+    this.header = this.page
+      .getByTestId('ibf-dashboard-interface')
+      .getByTestId('heading-display-name-span');
     this.countryLogos = this.page.getByTestId('logo-image');
     this.userLoggedInLabel = this.page.getByTestId('user-display-name');
     this.logOutButton = this.page.getByTestId('user-state-logout-button');
   }
 
-  async headerComponentIsVisible({ country: { name }, title }: Dataset) {
-    const header = this.header.filter({ hasText: `${title} ${name}` });
+  async headerComponentIsVisible({ title }: Dataset) {
+    const header = this.header.filter({ hasText: title });
     await expect(header).toBeVisible();
-  }
-
-  async headerComponentDisplaysCorrectDisasterType({
-    country: { name },
-    disasterType,
-    title,
-  }: {
-    country: Country;
-    disasterType: string;
-    title: string;
-  }) {
-    const header = this.header.filter({ hasText: `${title} ${name}` });
-    const headerText = await header.textContent().then((text) => text?.trim());
-    const headerTextTransformed = headerText?.replace('  ', ' '); // REFACTOR
-
-    expect(headerTextTransformed).toContain(`${title} ${name} ${disasterType}`);
   }
 
   async allUserStateElementsAreVisible({ firstName, lastName }: User) {
