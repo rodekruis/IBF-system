6c1824b59b25ebf1e98f119ff3aefa93bd8f313a
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-09-26 22:11:48 +0200

    feat: router animation

--- interfaces/IBF-dashboard/src/app/app.component.ts
+++ interfaces/IBF-dashboard/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { AnimationController, Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { LoaderService } from 'src/app/services/loader.service';
@@ -14,11 +14,35 @@ export class AppComponent implements OnDestroy {
   private loaderSubscription: Subscription;
   public loading = true;
   private defaultLanguage = 'en';
+  public routerAnimation = (
+    _baseEl: HTMLElement,
+    opts?: { leavingEl: HTMLElement; enteringEl: HTMLElement },
+  ) => {
+    const animationDuration = 200;
+    const leavingAnimation = this.animationController
+      .create()
+      .addElement(opts.leavingEl)
+      .duration(animationDuration)
+      .easing('ease-out')
+      .fromTo('opacity', 1, 0);
+    const enteringAnimation = this.animationController
+      .create()
+      .addElement(opts.enteringEl)
+      .duration(animationDuration)
+      .easing('ease-out')
+      .fromTo('opacity', 0, 1);
+
+    return this.animationController
+      .create()
+      .duration(animationDuration)
+      .addAnimation([leavingAnimation, enteringAnimation]);
+  };
 
   constructor(
     private platform: Platform,
     private loaderService: LoaderService,
     private translateService: TranslateService,
+    private animationController: AnimationController,
   ) {
     this.initializeApp();
 
