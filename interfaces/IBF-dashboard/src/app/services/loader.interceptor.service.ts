364c6dae6bb273095417fe36ff6c83ab1b06901e
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-11-05 09:23:09 +0100

    refactor: update deprecated method

--- interfaces/IBF-dashboard/src/app/services/loader.interceptor.service.ts
+++ interfaces/IBF-dashboard/src/app/services/loader.interceptor.service.ts
@@ -27,34 +27,34 @@ export class LoaderInterceptorService implements HttpInterceptor {
 
     if (skipRequest) {
       return next.handle(request).pipe(
-        tap(
-          (event: HttpEvent<unknown>) => {
+        tap({
+          next: (event: HttpEvent<unknown>) => {
             if (event instanceof HttpResponse) {
               this.onEnd(requestPath);
             }
           },
-          (error: unknown) => {
+          error: (error: unknown) => {
             console.log('error: ', error);
             this.onEnd(requestPath);
           },
-        ),
+        }),
       );
     }
 
     this.showLoader(requestPath);
 
     return next.handle(request).pipe(
-      tap(
-        (event: HttpEvent<unknown>) => {
+      tap({
+        next: (event: HttpEvent<unknown>) => {
           if (event instanceof HttpResponse) {
             this.onEnd(requestPath);
           }
         },
-        (error: unknown) => {
+        error: (error: unknown) => {
           console.log('error: ', error);
           this.onEnd(requestPath);
         },
-      ),
+      }),
     );
   }
 
