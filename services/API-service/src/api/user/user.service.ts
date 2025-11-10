e741182c79819b9b3db28f80a99da3924b3826e1
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-11-10 14:21:51 +0100

    style: format spacing

--- services/API-service/src/api/user/user.service.ts
+++ services/API-service/src/api/user/user.service.ts
@@ -36,7 +36,7 @@ export class UserService {
 
   private readonly relations: string[] = ['countries', 'disasterTypes'];
 
-  public constructor(private readonly lookupService: LookupService) { }
+  public constructor(private readonly lookupService: LookupService) {}
 
   public async findAll(): Promise<UserEntity[]> {
     return await this.userRepository.find({ relations: this.relations });
