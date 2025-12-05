a9e988faa4f5a893e9b85a5ab265f5dbaefbb12e
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-12-05 14:59:10 +0100

    fix: import error

--- interfaces/IBF-dashboard/src/app/mocks/alert-areas.mock.ts
+++ interfaces/IBF-dashboard/src/app/mocks/alert-areas.mock.ts
@@ -1,6 +1,6 @@
-import { AlertLevel } from 'src/app/services/event.service';
 import { AdminLevel } from 'src/app/types/admin-level';
 import { AlertArea } from 'src/app/types/alert-area';
+import { AlertLevel } from 'src/app/types/alert-level';
 
 export const MOCK_ALERT_AREAS: AlertArea[] = [
   {
